724684c5dcf24c1d84589015807ec83b093fbd5d
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-04-06 14:23:15 +0200

    Number form field

---
+++ src/Form/Fields/SharpFormNumberField.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Code16\Sharp\Form\Fields;
+
+use Code16\Sharp\Form\Fields\Utils\WithPlaceholder;
+
+class SharpFormNumberField extends SharpFormField
+{
+    use WithPlaceholder;
+
+    /**
+     * @var int
+     */
+    protected $min;
+
+    /**
+     * @var int
+     */
+    protected $max;
+
+    /**
+     * @var int
+     */
+    protected $step = 1;
+
+    /**
+     * @var bool
+     */
+    protected $showControls = false;
+
+    /**
+     * @param string $key
+     * @return static
+     */
+    public static function make(string $key)
+    {
+        return new static($key, 'number');
+    }
+
+    /**
+     * @param int $min
+     * @return $this
+     */
+    public function setMin(int $min)
+    {
+        $this->min = $min;
+
+        return $this;
+    }
+
+    /**
+     * @param int $max
+     * @return $this
+     */
+    public function setMax(int $max)
+    {
+        $this->max = $max;
+
+        return $this;
+    }
+
+    /**
+     * @param int $step
+     * @return $this
+     */
+    public function setStep(int $step)
+    {
+        $this->step = $step;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $showControls
+     * @return $this
+     */
+    public function setShowControls(bool $showControls = true)
+    {
+        $this->showControls = $showControls;
+
+        return $this;
+    }
+
+
+    /**
+     * @return array
+     */
+    protected function validationRules()
+    {
+        return [
+            "min" => "integer",
+            "max" => "integer",
+            "step" => "required|integer",
+            "showControls" => "required|bool",
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return parent::buildArray([
+            "min" => $this->min,
+            "max" => $this->max,
+            "step" => $this->step,
+            "showControls" => $this->showControls,
+            "placeholder" => $this->placeholder,
+        ]);
+    }
+}
