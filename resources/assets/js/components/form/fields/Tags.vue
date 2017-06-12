dae60ebedd31cf6e12b204217776ab0077d0679e
antoine <antoine.guingand@wanadoo.fr>
2017-06-12 19:52:17 +0200

    Fix tags remove not working

--- resources/assets/js/components/form/fields/Tags.vue
+++ resources/assets/js/components/form/fields/Tags.vue
@@ -1,17 +1,17 @@
 <template>
     <multiselect class="SharpTags"
-                 :value="value"
-                 :options="options"
+                 :value="tags"
+                 :options="indexedOptions"
                  :placeholder="placeholder"
                  :tag-placeholder="createText"
                  :max="maxTagCount"
                  :taggable="creatable"
                  :close-on-select="false"
-
+                 track-by="internalId"
                  label="label"
                  multiple searchable hide-selected
                  selectLabel="" selectedLabel="" deselectLabel=""
-                 @input="v=>$emit('input',v)"
+                 @input="handleInput"
                  @tag="handleNewTag">
         <template slot="maxElements">
             Maximum de {{maxTagCount}} éléments atteint
@@ -35,10 +35,41 @@
             createText:String,
             creatable:true,
         },
-        methods: {
-            handleNewTag(val) {
-                this.value.push({id:null,label:val});
+        data() {
+            return {
+                tags:[],
+                lastIndex:0
             }
+        },
+        computed: {
+            indexedOptions() {
+                return this.patch(this.options);
+            },
+        },
+        watch: {
+            tags(tags) {
+                if(this.lastIndex) {
+                    this.$emit('input',tags.map(t =>({id:t.id, label:t.label})));
+                }
+            }
+        },
+        methods: {
+            patch(array) {
+                return array.map((o,i) => {
+                    o.internalId = i;
+                    return o;
+                });
+            },
+            handleNewTag(val) {
+                this.tags.push({id:null,label:val,internalId:this.lastIndex++});
+            },
+            handleInput(val) {
+                this.tags = val;
+            }
+        },
+        mounted() {
+            this.tags = this.patch(this.value);
+            this.lastIndex = this.options.length + this.tags.length;
         }
     }
 </script>
