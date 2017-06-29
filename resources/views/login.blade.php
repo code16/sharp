8e92cf44c74cbf98e7d508f6435df6957768cca7
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-06-29 14:38:30 +0200

    Login page styling

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
             <form method="POST" action="{{ route("code16.sharp.login.post") }}">
                 {{ csrf_field() }}
                 <div class="row justify-content-center">
-                    <div class="col-sm-6 col-lg-4">
+                    <div class="col-sm-9 col-md-6 col-lg-5 col-xl-4">
 
                         <div class="SharpModule">
                             <div class="SharpModule__inner">
@@ -16,28 +16,22 @@
                                 </div>
                                 <div class="SharpModule__content">
                                     @if ($errors->any())
-                                    <div class="alert alert-danger">
-                                        @lang('sharp::auth.validation_error')
-                                    </div>
+                                        <div class="alert alert-danger">
+                                            @lang('sharp::auth.validation_error')
+                                        </div>
 
                                     @elseif (session()->has('invalid'))
-                                    <div class="alert alert-danger">
-                                        @lang('sharp::auth.invalid_credentials')
-                                    </div>
+                                        <div class="alert alert-danger">
+                                            @lang('sharp::auth.invalid_credentials')
+                                        </div>
                                     @endif
 
                                     <div class="SharpFieldContainer SharpForm--form-item">
-                                        <label class="SharpForm--label" for="login">
-                                            Login
-                                        </label>
-                                        <input type="text" name="login" id="login" class="SharpText" value="{{ old('login') }}">
+                                        <input type="text" name="login" id="login" class="SharpText" value="{{ old('login') }}" placeholder="login">
                                     </div>
 
                                     <div class="SharpFieldContainer SharpForm--form-item">
-                                        <label class="SharpForm--label" for="password">
-                                            Password
-                                        </label>
-                                        <input type="password" name="password" id="password" class="SharpText">
+                                        <input type="password" name="password" id="password" class="SharpText" placeholder="password">
                                     </div>
                                     <button type="submit" class="SharpButton SharpButton--primary">
                                         Login
