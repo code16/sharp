c3c60a38461b08bbc7861b1bd62ca85f2743e60f
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-07-20 12:06:05 +0200

    PhpUnit assertions for the Form + saturn test

--- saturn/config/database.php
+++ saturn/config/database.php
@@ -39,6 +39,12 @@ return [
             'prefix' => '',
         ],
 
+        'testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
