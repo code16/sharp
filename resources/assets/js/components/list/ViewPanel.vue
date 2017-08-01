cbcd52c2c93dc54de6aac463de255e2c078eeea2
antoine <antoine.guingand@wanadoo.fr>
2017-08-01 11:28:22 +0200

    View panel (WIP)

---
+++ resources/assets/js/components/list/ViewPanel.vue
@@ -0,0 +1,19 @@
+<template>
+    <transition enter-class="SharpViewPanel--collapsed"
+                enter-active-class="SharpViewPanel--expanding"
+                leave-active-class="SharpViewPanel--collapsing">
+        <div v-html="content" class="SharpViewPanel" v-show="show"></div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        model: {
+            prop:'show',
+            event:'change'
+        },
+        props: {
+
+        }
+    }
+</script>
