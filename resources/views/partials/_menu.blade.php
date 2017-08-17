45abfe82c43c90a152b0d37188802fa37435dded
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-08-17 11:21:27 +0200

    Localize menu dashboard + code format

--- resources/views/partials/_menu.blade.php
+++ resources/views/partials/_menu.blade.php
@@ -5,35 +5,36 @@
         <sharp-nav-item disabled>
             <span>
                 <i class="fa fa-user"></i>
-            {{ $sharpMenu->user }}
+                {{ $sharpMenu->user }}
             </span>
-            <a href="{{ route('code16.sharp.logout') }}" style="float:left"><i class="fa fa-sign-out"></i></a>
+            <a href="{{ route('code16.sharp.logout') }}"><i class="fa fa-sign-out"></i></a>
         </sharp-nav-item>
 
         @if($sharpMenu->dashboard)
-        <sharp-nav-item :current="{{ json_encode($dashboard ?? false) }}" link="{{ route('code16.sharp.dashboard') }}">
-            <span>
-                <i class="fa fa-dashboard"></i>
-
-                Dashboard {{-- <-- TODO i18 this --}}
-            </span>
-        </sharp-nav-item>
+            <sharp-nav-item :current="{{ json_encode($dashboard ?? false) }}" link="{{ route('code16.sharp.dashboard') }}">
+                <span>
+                    <i class="fa fa-dashboard"></i>
+                    @lang('sharp::menu.dashboard')
+                </span>
+            </sharp-nav-item>
         @endif
 
         @foreach($sharpMenu->categories as $category)
-        <sharp-collapsible-item label="{{ $category->label }}">
-            @foreach($category->entities as $entity)
-            <sharp-nav-item :current="{{ json_encode(($entityKey??false)==$entity->key) }}"
-                            link="{{ route('code16.sharp.list', $entity->key) }}">
-                <span>
-                    @if($entity->icon)
-                    <i class="fa {{ $entity->icon }}"></i>
-                    @endif
-                    {{ $entity->label }}
-                </span>
-            </sharp-nav-item>
-            @endforeach
-        </sharp-collapsible-item>
+            <sharp-collapsible-item label="{{ $category->label }}">
+
+                @foreach($category->entities as $entity)
+                    <sharp-nav-item :current="{{ json_encode(($entityKey??false)==$entity->key) }}"
+                                    link="{{ route('code16.sharp.list', $entity->key) }}">
+                        <span>
+                            @if($entity->icon)
+                                <i class="fa {{ $entity->icon }}"></i>
+                            @endif
+                            {{ $entity->label }}
+                        </span>
+                    </sharp-nav-item>
+                @endforeach
+
+            </sharp-collapsible-item>
         @endforeach
     </ul>
 </sharp-left-nav>
