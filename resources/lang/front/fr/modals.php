6c07b83941daa3712a23e76cc634572131825b55
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-09-05 15:16:41 +0200

    Add missing FR keys in locales

--- resources/lang/front/fr/modals.php
+++ resources/lang/front/fr/modals.php
@@ -6,5 +6,7 @@ return [
     'command.confirm.title' => 'Confirmer',
     'command.info.title' => 'Information',
     'state.422.title' => "Impossible de modifier l'état",
-    'cropper.title' => 'Transformer l’image'
+    'cropper.title' => 'Transformer l’image',
+    'ok_button' => 'Ok',
+    'cancel_button' => 'Annuler'
 ];
