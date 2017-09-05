7a6baf3572cb9e9c9f582191fe681ee8dc5f6357
antoine <antoine.guingand@wanadoo.fr>
2017-09-05 15:38:37 +0200

    Show dropdown above when not enough space

--- resources/assets/js/components/dropdown/Dropdown.vue
+++ resources/assets/js/components/dropdown/Dropdown.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="SharpDropdown"
-         :class="{'SharpDropdown--open':opened, 'SharpDropdown--disabled':disabled}"
+         :class="{'SharpDropdown--open':opened,
+         'SharpDropdown--disabled':disabled,
+         'SharpDropdown--above':isAbove}"
          :tabindex="disabled?-1:0"
-         @focus="handleFocus" @blur="opened=false">
+         @focus="handleFocus" @blur="handleBlur">
         <li class="SharpDropdown__text" @mousedown="toggleIfFocused">
             <slot name="text">{{text}}</slot>
         </li>
         <dropdown-arrow v-if="showArrow" class="SharpDropdown__arrow"></dropdown-arrow>
         <li v-if="!disabled">
-            <ul class="SharpDropdown__list">
+            <ul class="SharpDropdown__list" ref="list">
                 <slot></slot>
             </ul>
         </li>
@@ -38,7 +40,8 @@
         },
         data() {
             return {
-                opened: false
+                opened: false,
+                isAbove: false
             }
         },
         watch: {
@@ -49,15 +52,25 @@
             }
         },
         methods:{
-            handleFocus() {
+            async handleFocus() {
                 if(this.disabled) return;
                 this.opened = true;
+                await this.$nextTick();
+                this.adjustPosition();
+            },
+            handleBlur() {
+                this.opened = false;
+                this.isAbove = false;
             },
             toggleIfFocused(e) {
                 if(this.opened) {
                     this.$el.blur();
                     e.preventDefault();
                 }
+            },
+            adjustPosition () {
+                let { bottom } = this.$refs.list.getBoundingClientRect();
+                this.isAbove = bottom > window.innerHeight;
             }
         },
         mounted() {
