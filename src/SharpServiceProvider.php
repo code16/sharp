76cba738a0c5d06343d6f11045471159be943f60
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-09-07 13:56:01 +0200

    Version 4.0-BETA2

--- src/SharpServiceProvider.php
+++ src/SharpServiceProvider.php
@@ -25,7 +25,7 @@ class SharpServiceProvider extends ServiceProvider
     /**
      * @var string
      */
-    const VERSION = '4.0-BETA1';
+    const VERSION = '4.0-BETA2';
 
     public function boot()
     {
