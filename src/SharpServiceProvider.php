b53f8009257f2dbd900a59e1ea3b27fc36678c3b
Philippe Lonchampt <philippelonchampt@gmail.com>
2017-09-19 14:14:08 +0200

    Fix auth for dashboard

--- src/SharpServiceProvider.php
+++ src/SharpServiceProvider.php
@@ -64,7 +64,7 @@ class SharpServiceProvider extends ServiceProvider
         // Override Laravel's Gate to handle Sharp's ability to define a custom Guard
         $this->app->singleton(GateContract::class, function ($app) {
             return new \Illuminate\Auth\Access\Gate($app, function () use ($app) {
-                return request()->is("sharp/*")
+                return request()->is("sharp") || request()->is("sharp/*")
                     ? sharp_user()
                     : auth()->guard(config("auth.defaults.guard"))->user();
             });
