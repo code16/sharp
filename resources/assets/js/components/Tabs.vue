849ff17fcac8a7d2522d8d315bc455e255ccd5bc
antoine <antoine.guingand@wanadoo.fr>
2018-01-18 15:03:35 +0100

    Tabs dropdown error dot integration

--- resources/assets/js/components/Tabs.vue
+++ resources/assets/js/components/Tabs.vue
@@ -1,13 +1,16 @@
 <template>
     <div :id="id || null" class="SharpTabs" :class="{ 'SharpTabs--collapse':collapseActivated , 'SharpTabs--nav-overflow':hasNavOverflow }">
         <div class="mb-3" :class="{ 'm-sm-0':!hasNavOverflow  }">
-            <button class="SharpTabs__collapse-btn SharpButton SharpButton--sm SharpButton--secondary mb-1"
-                    :class="{ 'd-sm-none':!hasNavOverflow, 'd-none':!collapseActivated }"
+            <slot v-if="hasNavOverflow" name="nav-prepend"></slot>
+            <button class="SharpTabs__collapse-btn SharpButton SharpButton--secondary mb-1"
+                    :class="{ 'd-none':!hasNavOverflow }"
                     @click="expanded=!expanded"
             >
-                {{ tabs[currentTab] && tabs[currentTab].title }} <sharp-dropdown-arrow class="ml-1" :style="expanded && 'transform: rotate(180deg)'"/>
+                <span v-if="tabs[currentTab]" :class="dropdownButtonClasses">{{ tabs[currentTab].title }}</span>
+                <sharp-dropdown-arrow class="ml-1" :style="expanded && 'transform: rotate(180deg)'"/>
             </button>
             <div class="SharpTabs__nav SharpTabs__nav--ghost m-0 p-0" style="height:0;overflow: hidden" v-has-overflow.width="hasNavOverflow">
+                <div :style="{minWidth:`${extraNavGhostWidth}px`}">&nbsp;</div>
                 <a v-for="tab in tabs" class="SharpTabs__nav-link" v-html="tab.title"></a>
             </div>
             <b-collapse id="tabs" :visible="expanded" :class="{ 'd-block':!hasNavOverflow }">
@@ -24,6 +27,7 @@
                      @keydown.shift.right="setTab(tabs.length,false,-1)"
                      @keydown.shift.down="setTab(tabs.length,false,-1)"
                 >
+                    <slot v-if="!hasNavOverflow" name="nav-prepend"></slot>
                     <a v-for="(tab, index) in tabs"
                        class="SharpTabs__nav-link"
                        :class="{'SharpTabs__nav-link--has-error':tab.hasError,
@@ -57,9 +61,13 @@
     import bCollapse from 'bootstrap-vue/es/components/collapse/collapse';
     import SharpDropdownArrow from './dropdown/Arrow.vue';
     import HasOverflow from '../directives/HasOverflow';
+    import { Responsive } from '../mixins';
 
     export default {
         name:'SharpBTabs',
+
+        mixins: [Responsive('sm')],
+
         extends: Tabs,
         components: {
             bCollapse,
@@ -69,23 +77,38 @@
         data() {
             return {
                 expanded: true,
-                hasNavOverflow: false
+                hasNavOverflow: false,
+                extraNavGhostWidth: 0
             }
         },
         watch: {
             currentTab() {
-                if(this.collapseActivated) {
+                if(this.hasNavOverflow) {
                     this.expanded = false;
                 }
             }
         },
         computed: {
             collapseActivated() {
-                return this.hasNavOverflow || this.tabs.length>2;
+                return this.isViewportSmall && (this.hasNavOverflow || this.tabs.length>2);
+            },
+            tabsHaveError() {
+                return this.tabs.some(tab => tab.hasError);
+            },
+            dropdownButtonClasses() {
+                return this.tabs[this.currentTab].hasError ? 'error-dot' : this.tabsHaveError ? 'error-dot--partial' : '';
+            }
+        },
+        methods : {
+            addExtraNavGhostWidthForSlot(name) {
+                let $slot = this.$slots[name];
+                if($slot && $slot[0] && $slot[0].elm) {
+                    this.extraNavGhostWidth += $slot[0].elm.offsetWidth;
+                }
             }
         },
         mounted() {
-
+            this.addExtraNavGhostWidthForSlot('nav-prepend');
         },
         directives: {
             HasOverflow
