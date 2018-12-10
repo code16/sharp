114bc8b11fe2314991a880cd0cae5fc2e1dd06e3
Philippe Lonchampt <philippelonchampt@gmail.com>
2018-12-10 18:15:33 +0100

    Assets

--- resources/assets/dist/client-api.js
+++ resources/assets/dist/client-api.js
@@ -1,1 +1,23 @@
-webpackJsonp([2],{2:function(e,n,o){e.exports=o("pOC0")},pOC0:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var t=o("I3G/"),p=o.n(t);window.Vue=p.a}},[2]);
+webpackJsonp([2],{
+
+/***/ "./resources/assets/js/client-api.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__("./node_modules/vue/dist/vue.common.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_vue__);
+
+window.Vue = __WEBPACK_IMPORTED_MODULE_0_vue___default.a;
+
+/***/ }),
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./resources/assets/js/client-api.js");
+
+
+/***/ })
+
+},[2]);
