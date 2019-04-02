0d0d29e08f696507b32ad69fd89702c806943807
antoine <antoine.guingand@wanadoo.fr>
2019-04-02 15:46:30 +0200

    Separate dynamic attributes functions into modules handle default attributes

--- resources/assets/js/__tests__/dynamic-attributes.test.js
+++ resources/assets/js/__tests__/dynamic-attributes.test.js
@@ -1,20 +1,27 @@
-import * as dynamicAttributes from '../components/form/dynamic-attributes';
 import * as dynamicAttributesUtil from '../components/form/dynamic-attributes/util';
+import * as dynamicAttributesResolve from '../components/form/dynamic-attributes/resolve';
 
 import {
+    transformAttributes
+} from "../components/form/dynamic-attributes";
+
+import {
+    resolveValue,
+} from '../components/form/dynamic-attributes/resolve';
+
+import {
+    getEmptyValueSources,
     getDynamicAttributeOptions,
     getValueFromMap,
     getValueFromTemplate,
     getSourcesFromTemplate,
-    resolveValue
-} from '../components/form/dynamic-attributes';
-
-import {
-    getEmptySources,
-    warnEmptyValue,
 } from '../components/form/dynamic-attributes/util';
 
 describe('Dynamic attributes', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
     test('getDynamicAttributesOptions', () => {
         expect(getDynamicAttributeOptions()).toBeUndefined();
         expect(getDynamicAttributeOptions([{ name:'attribute' }], 'other')).toBeUndefined();
@@ -42,14 +49,24 @@ describe('Dynamic attributes', () => {
         ).toEqual('value');
     });
 
-    xtest('getValueFromTemplate', () => {
+    test('getValueFromTemplate', () => {
         expect(getValueFromTemplate({
             template: '{{name}}/{{year}}',
             contextData: {
                 name: 'George',
                 year: 2019,
-            }
+            },
+            sources: ['name', 'year'],
         })).toBe('George/2019');
+    });
+    test('getValueFromTemplate should not throw', () => {
+        expect(getValueFromTemplate({
+            template: '{{name}}/{{year}}',
+            contextData: {
+                name: 'George',
+            },
+            sources: ['name', 'year'],
+        })).toBe('George/');
     });
 
     test('getSourcesFromTemplate', () => {
@@ -57,37 +74,132 @@ describe('Dynamic attributes', () => {
             .toEqual(['name', 'year', 'empty']);
     });
 
+    test('getEmptyValueSources', () => {
+        expect(getEmptyValueSources({
+            contextSources: ['field1', 'field2', 'field3', 'field4', 'field5'],
+            contextData: {
+                field1: '',
+                field2: null,
+                field3: undefined,
+                field4: 0,
+                field5: 'aaa'
+            },
+        })).toEqual(['field1', 'field2', 'field3']);
+    });
+
     describe('resolveValue', () => {
         beforeEach(() => {
+            jest.spyOn(dynamicAttributesUtil, 'getEmptyValueSources').mockImplementation(()=>[]);
+        });
 
-            jest.spyOn(dynamicAttributesUtil, 'warnEmptyValue').mockImplementation();
-            jest.spyOn(dynamicAttributesUtil, 'getEmptySources').mockImplementation(()=>[]);
+        test('resolve not dynamic', () => {
+            jest.spyOn(dynamicAttributesUtil, 'getDynamicAttributeOptions').mockImplementation();
+            expect(
+                resolveValue('attr', 'attrValue', {
+                    dynamicAttributes: [{ name:'attr', }],
+                })
+            ).toEqual({ value:'attrValue' });
+            expect(getDynamicAttributeOptions).toHaveBeenCalledWith([{ name:'attr', }], 'attr');
         });
-        afterEach(() => {
-            jest.restoreAllMocks();
-        });
-        test('map', () => {
-            jest.spyOn(dynamicAttributes, 'getValueFromMap').mockImplementation();
-            jest.spyOn(dynamicAttributes, 'getDynamicAttributeOptions').mockImplementation(()=>({
+
+        test('resolve map', () => {
+            jest.spyOn(dynamicAttributesUtil, 'getValueFromMap').mockImplementation(()=>'resolvedValue');
+            jest.spyOn(dynamicAttributesUtil, 'getDynamicAttributeOptions').mockImplementation(()=>({
                 name: 'options',
                 type: 'map',
                 path: ['brand_name'],
             }));
-            dynamicAttributes.resolveValue({
-                sourceName: 'field',
-                attributeName: 'options',
-                attributeValue: { 1:[] },
-                contextData: {
-                    brand_name: 'Renault',
-                }
-            });
-            expect(dynamicAttributes.getValueFromMap).toHaveBeenCalledWith({
+            expect(
+                resolveValue('options', { 1:[] }, {
+                    contextData: {
+                        brand_name: 'Renault',
+                    },
+                })
+            ).toEqual({ value:'resolvedValue' });
+            expect(getValueFromMap).toHaveBeenCalledWith({
                 map: { 1:[] },
                 path: ['brand_name'],
                 contextData: {
                     brand_name: 'Renault',
                 }
             });
+        });
+
+        test('resolve template', () => {
+            jest.spyOn(dynamicAttributesUtil, 'getValueFromTemplate').mockImplementation(()=>'resolvedValue');
+            jest.spyOn(dynamicAttributesUtil, 'getSourcesFromTemplate').mockImplementation(()=>['url']);
+            jest.spyOn(dynamicAttributesUtil, 'getDynamicAttributeOptions').mockImplementation(()=>({
+                name: 'url',
+                type: 'template',
+            }));
+            expect(
+                resolveValue('url', '/api/{{brand_name}}', {
+                    contextData: {
+                        year: 2016,
+                    },
+                })
+            ).toEqual({ value: 'resolvedValue' });
+            expect(getSourcesFromTemplate).toHaveBeenCalledWith('/api/{{brand_name}}');
+            expect(getValueFromTemplate).toHaveBeenCalledWith({
+                template: '/api/{{brand_name}}',
+                sources: ['url'],
+                contextData: {
+                    year: 2016,
+                },
+            });
+        });
+
+        test('resolve default', () => {
+            jest.spyOn(dynamicAttributesUtil, 'getEmptyValueSources').mockImplementation(()=>(['field']));
+            jest.spyOn(dynamicAttributesUtil, 'getDynamicAttributeOptions').mockImplementation(()=>({
+                default: 'defaultValue',
+            }));
+            expect(resolveValue('attr', 'value', {})).toEqual({ isDefault: true, value:'defaultValue', });
+        });
+    });
+
+    test('transformAttributes', () => {
+        jest.spyOn(dynamicAttributesResolve, 'resolveValue').mockImplementation(() => ({ value:'resolvedValue' }));
+        expect(
+            transformAttributes({
+                localValues: [],
+                placeholder: 'Enter text',
+            }, [
+                { name:'localValues' },
+            ], {
+                autocomplete: 4,
+            })
+        ).toEqual({
+            attributes: {
+                localValues: 'resolvedValue',
+                placeholder: 'resolvedValue',
+            },
+            resolvedDefaultAttributes: [],
+        });
+
+        expect(resolveValue).toHaveBeenCalledWith('localValues', [], {
+            dynamicAttributes: [{ name:'localValues' }],
+            contextData: {
+                autocomplete: 4,
+            },
+        });
+    });
+
+    test('transformAttributes with defaults', () => {
+        jest.spyOn(dynamicAttributesResolve, 'resolveValue').mockImplementation(()=>({
+            value:'defaultValue', isDefault: true
+        }));
+        expect(
+            transformAttributes({
+                localValues: [],
+                placeholder: 'Enter text',
+            }, null, null)
+        ).toEqual({
+            attributes: {
+                localValues: 'defaultValue',
+                placeholder: 'defaultValue',
+            },
+            resolvedDefaultAttributes: ['localValues', 'placeholder'],
         });
     });
 });
--- resources/assets/js/components/form/dynamic-attributes/util.js
+++ resources/assets/js/components/form/dynamic-attributes/util.js
@@ -1,6 +1,10 @@
+import get from 'lodash/get';
+import buildTemplate from 'lodash/template';
 import { error } from "../../../util";
 
-export function getEmptySources({ contextSources, contextData }) {
+const interpolateRE = /{{([\s\S]+?)}}/g;
+
+export function getEmptyValueSources({ contextSources, contextData }) {
     return contextSources.filter(sourceKey => {
         const value = contextData[sourceKey];
         return value == null || value === '';
@@ -13,5 +17,32 @@ export function warnEmptyValue(emptySources, { sourceName, attributeName }) {
                The field '${sourceKey}' has value null, undefined or empty string.\n 
                Ensure the field not clearable and has initial default value.\n
                You can also define a default value for the attribute.`);
+    });
+}
+
+export function getDynamicAttributeOptions(dynamicAttributes, attributeName) {
+    return (dynamicAttributes || [])
+        .find(option => option.name === attributeName);
+}
+
+export function getValueFromMap({ map, path, contextData }) {
+    const valuePath = path.map(key => contextData[key]);
+    return get(map, valuePath);
+}
+
+export function getSourcesFromTemplate(template) {
+    return [...template.matchAll(interpolateRE)].map(match => match[1].trim());
+}
+
+export function getValueFromTemplate({ template, sources, contextData }) {
+    const compile = buildTemplate(template, {
+        interpolate: interpolateRE,
+        evaluate: false,
+        escape: false,
+    });
+    const defaultData = (sources||[]).reduce((res, key) => ({ ...res, [key]: null, }), {});
+    return compile({
+        ...defaultData,
+        ...contextData,
     });
 }
