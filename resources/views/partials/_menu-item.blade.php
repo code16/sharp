20ef9cc15524e7d84cf7ebae1fdfcc702eed8271
antoine <antoine.guingand@wanadoo.fr>
2019-06-18 12:00:16 +0200

    Menu item text wrapping, closes #141

--- resources/views/partials/_menu-item.blade.php
+++ resources/views/partials/_menu-item.blade.php
@@ -1,9 +1,11 @@
 <sharp-nav-item :current="{{ json_encode($isCurrent) }}" link="{{ $item->url }}">
     <div class="row d-inline-flex no-gutters flex-nowrap w-100">
-        <div class="col text-truncate">
-            @if($item->icon)
+        @if($item->icon)
+            <div class="col-auto mr-1">
                 <sharp-item-visual :item="{{ json_encode($item) }}" icon-class="fa-fw"></sharp-item-visual>
-            @endif
+            </div>
+        @endif
+        <div class="col">
             {{ $item->label }}
         </div>
         @if($item->type === 'url')
