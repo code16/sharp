97d5f10f0a570214135a17e3ff7d750539b9c748
Philippe Lonchampt <philippelonchampt@gmail.com>
2019-06-18 12:25:58 +0200

    Version

--- src/SharpServiceProvider.php
+++ src/SharpServiceProvider.php
@@ -26,7 +26,7 @@ use Intervention\Image\ImageServiceProviderLaravel5;
 class SharpServiceProvider extends ServiceProvider
 {
     /** @var string */
-    const VERSION = '4.1.13';
+    const VERSION = '4.1.14';
 
     public function boot()
     {
