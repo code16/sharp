d41db414ce37422795cbcfe2b18f8e7c7c7e1935
Arnaud Becher <arnaud.becher@gmail.com>
2019-07-16 15:16:01 +0200

    fix list widget constructor

--- src/Dashboard/Widgets/SharpListWidget.php
+++ src/Dashboard/Widgets/SharpListWidget.php
@@ -4,6 +4,16 @@ namespace Code16\Sharp\Dashboard\Widgets;
 
 class SharpListWidget extends SharpWidget
 {
+    /**
+     * @param string $key
+     * @return static
+     */
+    public static function make(string $key)
+    {
+        $widget = new static($key, 'list');
+
+        return $widget;
+    }
 
     /**
      * @return array
