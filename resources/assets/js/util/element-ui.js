2d799e4c791b77d7ed015c08670b6fee31f97a09
Stephan Deglow <stephandeglow@gmail.com>
2019-10-02 10:40:53 +0200

    load element-ui german locales

--- resources/assets/js/util/element-ui.js
+++ resources/assets/js/util/element-ui.js
@@ -2,9 +2,10 @@ import fr from 'element-ui/lib/locale/lang/fr';
 import ru from 'element-ui/lib/locale/lang/ru-RU';
 import es from 'element-ui/lib/locale/lang/es';
 import en from 'element-ui/lib/locale/lang/en';
+import de from 'element-ui/lib/locale/lang/de';
 
 const languages = {
-    fr, ru, es, en,
+    fr, ru, es, en, de
 };
 
 export function elLang() {
