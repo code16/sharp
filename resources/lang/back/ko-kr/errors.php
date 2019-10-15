001972ce2bd66868d54f7c77a2b432e603bb2f87
Israel Araujo <israaraujo@gmail.com>
2019-10-15 15:59:09 +0900

    Korean initial translation

---
+++ resources/lang/back/ko-kr/errors.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'file_not_found' => '이 파일을 찾을 수 없습니다.',
+
+];
---
+++ resources/lang/back/ko-kr/menu.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'dashboard' => '대시 보드',
+    'no-dashboard-message' => "샤프에 환영합니다!"
+
+];
