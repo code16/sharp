dbcaae9320307fe4bd1d39841bcf6a6d56ebf269
Philippe Lonchampt <philippelonchampt@gmail.com>
2020-03-26 14:18:44 +0100

    Generalize DownloadController to SingleForm, Show, SingleShow (WIP)

--- src/Http/Api/DownloadController.php
+++ src/Http/Api/DownloadController.php
@@ -2,15 +2,13 @@
 
 namespace Code16\Sharp\Http\Api;
 
-use Code16\Sharp\Form\Fields\Utils\SharpFormFieldWithUpload;
+use Code16\Sharp\Http\SharpContext;
 use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Support\Str;
 
-class FormDownloadController extends ApiController
+class DownloadController extends ApiController
 {
-    /**
-     * @var FilesystemManager
-     */
+    /** @var FilesystemManager */
     protected $filesystem;
 
     /**
@@ -18,25 +16,25 @@ class FormDownloadController extends ApiController
      */
     public function __construct(FilesystemManager $filesystem)
     {
+        parent::__construct();
+        
         $this->filesystem = $filesystem;
     }
 
     /**
+     * @param string $fieldKey
      * @param string $entityKey
-     * @param string $instanceId
-     * @param string $formUploadFieldKey
-     * @return \Illuminate\Http\JsonResponse
+     * @param string|null $instanceId
+     * @return \Illuminate\Http\Response
      * @throws \Code16\Sharp\Exceptions\Auth\SharpAuthorizationException
      * @throws \Code16\Sharp\Exceptions\SharpInvalidEntityKeyException
      */
-    public function show($entityKey, $instanceId, $formUploadFieldKey)
+    public function show($fieldKey, $entityKey, $instanceId = null)
     {
         sharp_check_ability("view", $entityKey, $instanceId);
-
-        $form = $this->getFormInstance($entityKey);
-
+        
         list($disk, $path) = $this->determineDiskAndFilePath(
-            request('fileName'), $instanceId, $form->findFieldByKey($formUploadFieldKey)
+            request('fileName'), $fieldKey, $entityKey, $instanceId
         );
 
         abort_if(!$disk->exists($path), 404, trans("sharp::errors.file_not_found"));
@@ -51,13 +49,20 @@ class FormDownloadController extends ApiController
 
     /**
      * @param string $fileName
-     * @param string $instanceId
-     * @param SharpFormFieldWithUpload $field
+     * @param string $fieldKey
+     * @param string $entityKey
+     * @param null $instanceId
      * @return array
+     * @throws \Code16\Sharp\Exceptions\SharpInvalidEntityKeyException
      */
-    protected function determineDiskAndFilePath(string $fileName, $instanceId, $field)
+    protected function determineDiskAndFilePath(string $fileName, string $fieldKey, string $entityKey, $instanceId = null)
     {
-        $basePath = str_replace('{id}', $instanceId, $field->storageBasePath());
+        $field = $this->getField($entityKey, $fieldKey);
+        
+        $basePath = $instanceId 
+            ? str_replace('{id}', $instanceId, $field->storageBasePath())
+            : $field->storageBasePath();
+        
         $storageDisk = $field->storageDisk();
 
         if(strpos($fileName, ":") !== false) {
@@ -75,4 +80,18 @@ class FormDownloadController extends ApiController
         return [$disk, "$basePath/$fileName"];
     }
 
+    /**
+     * @param string $entityKey
+     * @param string $fieldKey
+     * @return \Code16\Sharp\Form\Fields\SharpFormField|\Code16\Sharp\Show\Fields\SharpShowField
+     * @throws \Code16\Sharp\Exceptions\SharpInvalidEntityKeyException
+     */
+    protected function getField(string $entityKey, string $fieldKey)
+    {
+        $fieldContainer = app(SharpContext::class)->isForm()
+            ? $this->getFormInstance($entityKey)
+            : $this->getShowInstance($entityKey);
+        
+        return $fieldContainer->findFieldByKey($fieldKey);
+    }
 }
---
+++ tests/Feature/Api/DownloadControllerTest.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace Code16\Sharp\Tests\Feature\Api;
+
+use Code16\Sharp\Form\Fields\SharpFormListField;
+use Code16\Sharp\Form\Fields\SharpFormUploadField;
+use Code16\Sharp\Form\SharpForm;
+use Code16\Sharp\Show\Fields\SharpShowFileField;
+use Code16\Sharp\Show\SharpShow;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class DownloadControllerTest extends BaseApiTest
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        Storage::fake("local");
+
+        $this->app['config']->set('sharp.entities.download.form', DownloadControllerTestForm::class);
+        $this->app['config']->set('sharp.entities.download.show', DownloadControllerTestShow::class);
+
+        $this->login();
+    }
+
+    /** @test */
+    function we_can_download_a_file_from_a_form_field()
+    {
+        $file = UploadedFile::fake()->image('test.jpg', 600, 600);
+        $file->storeAs('/files', 'test.jpg', ['disk' => 'local']);
+
+        $response = $this
+            ->getJson(
+                route('code16.sharp.api.form.download', [
+                    'formUploadFieldKey' => 'file',
+                    'entityKey' => 'download',
+                    'instanceId' => 1,
+                    'fileName' => 'test.jpg'
+                ])
+            )
+            ->assertStatus(200);
+        
+        $this->assertStringEqualsFile(
+            Storage::disk('local')->path('files/test.jpg'), 
+            $response->content()
+        );
+    }
+
+    /** @test */
+    function we_can_download_a_file_from_a_show_field()
+    {
+        $this->withoutExceptionHandling();
+        
+        $file = UploadedFile::fake()->image('test.jpg', 600, 600);
+        $file->storeAs('/files', 'test.jpg', ['disk' => 'local']);
+
+        $response = $this
+            ->getJson(
+                route('code16.sharp.api.show.download', [
+                    'formUploadFieldKey' => 'file',
+                    'entityKey' => 'download',
+                    'instanceId' => 1,
+                    'fileName' => 'test.jpg'
+                ])
+            )
+            ->assertStatus(200);
+
+        $this->assertStringEqualsFile(
+            Storage::disk('local')->path('files/test.jpg'),
+            $response->content()
+        );
+    }
+
+    /** @test */
+    function we_can_download_a_file_from_a_single_form_field()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /** @test */
+    function we_can_download_a_file_from_a_single_show_field()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /** @test */
+    function we_can_download_a_file_with_a_relative_path()
+    {
+        $file = UploadedFile::fake()->image('test.jpg', 600, 600);
+        $file->storeAs('/files', 'test.jpg', ['disk' => 'local']);
+
+        $this
+            ->getJson(
+                route('code16.sharp.api.form.download', [
+                    'formUploadFieldKey' => 'file',
+                    'entityKey' => 'download',
+                    'instanceId' => 1,
+                    'fileName' => 'test.jpg'
+                ])
+            )
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    function we_get_a_404_for_a_missing_file()
+    {
+        $this
+            ->getJson(
+                route('code16.sharp.api.form.download', [
+                    'formUploadFieldKey' => 'file',
+                    'entityKey' => 'download',
+                    'instanceId' => 1,
+                    'fileName' => 'test.jpg'
+                ])
+            )
+            ->assertStatus(404);
+    }
+
+    /** @test */
+    function we_can_download_a_file_in_a_list()
+    {
+        $file = UploadedFile::fake()->image('test.jpg', 600, 600);
+        $file->storeAs('/list-files', 'test.jpg', ['disk' => 'local']);
+
+        $response = $this->postJson(
+            route('code16.sharp.api.form.download', [
+                'entityKey' => 'download',
+                'instanceId' => 1,
+                'formUploadFieldKey' => 'list.file'
+            ]),
+            ['fileName' => 'test.jpg']
+        )->assertStatus(200);
+
+        $this->assertStringEqualsFile(storage_path('app/list-files/test.jpg'), $response->content());
+    }
+}
+
+class DownloadControllerTestForm extends SharpForm
+{
+    function buildFormFields()
+    {
+        $this->addField(
+            SharpFormUploadField::make("file")
+                ->setStorageDisk("local")
+                ->setStorageBasePath("files")
+        )->addField(
+            SharpFormListField::make("list")
+                ->addItemField(
+                    SharpFormUploadField::make("file")
+                        ->setStorageDisk("local")
+                        ->setStorageBasePath("list-files")
+                )
+        );
+    }
+    function buildFormLayout()
+    {
+    }
+    function find($id): array
+    {
+    }
+    function update($id, array $data): bool
+    {
+    }
+    function delete($id): bool
+    {
+    }
+    function create(): array
+    {
+    }
+}
+
+class DownloadControllerTestShow extends SharpShow
+{
+    function buildShowFields()
+    {
+        $this->addField(
+            SharpShowFileField::make("file")
+//                ->setStorageDisk("local")
+//                ->setStorageBasePath("files")
+        );/*->addField(
+            SharpFormListField::make("list")
+                ->addItemField(
+                    SharpFormUploadField::make("file")
+                        ->setStorageDisk("local")
+                        ->setStorageBasePath("list-files")
+                )
+        );*/
+    }
+    function find($id): array
+    {
+    }
+    function buildShowLayout()
+    {
+    }
+}
