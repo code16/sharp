7b9d7efdeac201753ddccbcc958331d6963fe73f
Philippe Lonchampt <philippelonchampt@gmail.com>
2020-03-30 12:44:10 +0200

    Fix test assertion

--- src/Utils/Testing/SharpAssertions.php
+++ src/Utils/Testing/SharpAssertions.php
@@ -2,7 +2,7 @@
 
 namespace Code16\Sharp\Utils\Testing;
 
-use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Testing\TestResponse;
 use PHPUnit\Framework\Assert as PHPUnit;
 
 trait SharpAssertions
