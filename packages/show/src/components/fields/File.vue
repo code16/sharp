c9d6b59d14a1a804836892eac5a28220772db26f
antoine <antoine.guingand@wanadoo.fr>
2020-04-02 12:03:58 +0200

    Show page: Improve list UI with files

--- packages/show/src/components/fields/File.vue
+++ packages/show/src/components/fields/File.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="ShowFileField">
         <div class="row mx-n2">
-            <template v-if="thumbnailUrl">
-                <div class="col-3 px-2 align-self-center" style="max-width: 125px">
+            <div class="col-3 px-2 align-self-center ShowFileField__thumbnail-col" :class="thumbnailColClasses">
+                <template v-if="hasThumbnail">
                     <div class="ShowFileField__thumbnail-container">
                         <img
                             class="ShowFileField__thumbnail"
@@ -13,36 +13,40 @@
                             @load="handleThumbnailLoaded"
                         >
                     </div>
-                </div>
-            </template>
-            <template v-else>
-                <div class="col-auto px-2">
-                    <div class="ShowFileField__placeholder">
+                </template>
+                <template v-else>
+                    <div class="ShowFileField__placeholder text-center">
                         <i class="fa far" :class="iconClass"></i>
                     </div>
-                </div>
-            </template>
+                </template>
+            </div>
             <div class="col px-2" style="min-width: 0">
-                <div class="ShowFileField__name text-truncate mb-2">
-                    {{ fileName }}
-                </div>
-                <div class="row mx-n2">
-                    <div class="col px-2">
-                        <div class="ShowFileField__size text-muted">
-                            {{ sizeLabel }}
-                        </div>
+                <div class="d-flex flex-column h-100">
+                    <div class="ShowFileField__name text-truncate mb-2">
+                        {{ fileName }}
                     </div>
-                    <div class="col-auto px-2">
-                        <Button
-                            class="ShowFileField__download-button"
-                            :href="downloadUrl"
-                            :download="fileName"
-                            type="primary"
-                            outline
-                            small
-                        >
-                            {{ lang('show.file.download') }}
-                        </Button>
+                    <div class="flex-fill">
+                        <div class="row mx-n2 h-100">
+                            <div class="col px-2">
+                                <div class="ShowFileField__size text-muted">
+                                    {{ sizeLabel }}
+                                </div>
+                            </div>
+                            <div class="col-auto px-2">
+                                <div class="ShowFileField__download-container h-100">
+                                    <Button
+                                        class="ShowFileField__download-button"
+                                        :href="downloadUrl"
+                                        :download="fileName"
+                                        type="primary"
+                                        outline
+                                        small
+                                    >
+                                        {{ lang('show.file.download') }}
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -68,6 +72,11 @@
             fieldConfigIdentifier: String,
             value: Object,
             label: String,
+            collapsed: {
+                type: Boolean,
+                default: true,
+            },
+            list: Boolean,
         },
         data() {
             return {
@@ -79,6 +88,11 @@
                 'entityKey',
                 'instanceId',
             ]),
+            thumbnailColClasses() {
+                return {
+                    'ShowFileField__thumbnail-col--collapsed': !this.hasThumbnail && this.collapsed,
+                }
+            },
             downloadUrl() {
                 return downloadFileUrl({
                     entityKey: this.entityKey,
@@ -93,6 +107,9 @@
             fileName() {
                 const parts = (this.name || '').split('/');
                 return parts[parts.length - 1];
+            },
+            hasThumbnail() {
+                return  this.value.thumbnail;
             },
             thumbnailUrl() {
                 return this.value ? this.value.thumbnail : null;
