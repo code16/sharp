8ec8159f8a35b6c11761921c005c4acc8e4c3ea8
Arnaud Becher <arnaud.becher@gmail.com>
2020-06-23 13:51:46 +0200

    init new login message

---
+++ saturn/resources/views/sharp/_login-page-message.blade.php
@@ -0,0 +1,14 @@
+
+<div role="alert" class="SharpNotification SharpNotification--info">
+    <div class="SharpNotification__details">
+        <div class="SharpNotification__text-wrapper">
+            <p class="SharpNotification__subtitle">
+                Use these accounts to login:
+                <ul>
+                    <li>- admin@example.com / secret</li>
+                    <li>- boss@example.com / secret <span style="font-style: italic">(has a few more permissions)</span></li>
+                </ul>
+            </p>
+        </div>
+    </div>
+</div>
