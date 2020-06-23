b1226229cc0c3e09dfc2b27798bca0857dd99acf
philippe <philippe@code16.fr>
2020-06-23 09:41:11 +0200

    Fix upload tests

--- tests/Unit/Form/Fields/Formatters/UploadFormatterTest.php
+++ tests/Unit/Form/Fields/Formatters/UploadFormatterTest.php
@@ -6,6 +6,7 @@ use Code16\Sharp\Exceptions\Form\SharpFormFieldFormattingMustBeDelayedException;
 use Code16\Sharp\Form\Fields\Formatters\UploadFormatter;
 use Code16\Sharp\Form\Fields\SharpFormUploadField;
 use Code16\Sharp\Tests\SharpTestCase;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManager;
 
@@ -48,43 +49,53 @@ class UploadFormatterTest extends SharpTestCase
     /** @test */
     function we_store_newly_uploaded_file_from_front()
     {
-        Storage::disk('local')->put('tmp/toto', 'toto');
+        $uploadedFile = UploadedFile::fake()
+            ->image("image.jpg");
+        
+        $uploadedFile->storeAs('/tmp', 'image.jpg', ['disk' => 'local']);
 
-        $formatter = new UploadFormatter;
         $field = SharpFormUploadField::make("upload")
             ->setStorageDisk("local");
-
-        $this->assertEquals([
-            "file_name" => "data/toto",
-            "size" => 4,
-            "mime_type" => 'text/plain',
-            "disk" => "local",
-            "transformed" => false
-        ], $formatter->fromFront(
-            $field, "attribute", [
-                "name" => "toto",
-                "uploaded" => true
-            ])
+        
+        $this->assertEquals(
+            [
+                "file_name" => "data/image.jpg",
+                "size" => $uploadedFile->getSize(),
+                "mime_type" => 'image/jpeg',
+                "disk" => "local",
+                "transformed" => false
+            ], 
+            (new UploadFormatter)->fromFront(
+                $field, 
+                "attribute", 
+                [
+                    "name" => "/image.jpg",
+                    "uploaded" => true
+                ]
+            )
         );
 
-        Storage::disk('local')->assertExists("data/toto");
+        Storage::disk('local')->assertExists("data/image.jpg");
     }
 
     /** @test */
     function we_delay_execution_if_the_storage_path_contains_instance_id_in_a_store_case()
     {
-        Storage::disk('local')->put('tmp/toto', 'toto');
+        UploadedFile::fake()
+            ->image("image.jpg")
+            ->storeAs('/tmp', 'image.jpg', ['disk' => 'local']);
 
-        $formatter = new UploadFormatter;
         $field = SharpFormUploadField::make("upload")
             ->setStorageDisk("local")
             ->setStorageBasePath("data/Test/{id}");
 
         $this->expectException(SharpFormFieldFormattingMustBeDelayedException::class);
 
-        $formatter->fromFront(
-            $field, "attribute", [
-                "name" => "toto",
+        (new UploadFormatter)->fromFront(
+            $field, 
+            "attribute", 
+            [
+                "name" => "/image.jpg",
                 "uploaded" => true
             ]
         );
@@ -93,126 +104,87 @@ class UploadFormatterTest extends SharpTestCase
     /** @test */
     function if_the_storage_path_contains_instance_id_in_an_update_case_we_replace_the_id_placeholder()
     {
-        Storage::disk('local')->put('tmp/toto', 'toto');
+        UploadedFile::fake()
+            ->image("image.jpg")
+            ->storeAs('/tmp', 'image.jpg', ['disk' => 'local']);
 
-        $formatter = new UploadFormatter;
         $field = SharpFormUploadField::make("upload")
             ->setStorageDisk("local")
             ->setStorageBasePath("data/Test/{id}");
 
-        $this->assertArrayContainsSubset([
-            "file_name" => "data/Test/50/toto"
-        ], $formatter->setInstanceId(50)->fromFront(
-            $field, "attribute", [
-                "name" => "toto",
-                "uploaded" => true
-            ]
-        ));
+        $this->assertArrayContainsSubset(
+            ["file_name" => "data/Test/50/image.jpg"], 
+            (new UploadFormatter)->setInstanceId(50)->fromFront(
+                $field, 
+                "attribute", 
+                [
+                    "name" => "image.jpg",
+                    "uploaded" => true
+                ]
+            )
+        );
 
-        Storage::disk('local')->assertExists("data/Test/50/toto");
+        Storage::disk('local')->assertExists("data/Test/50/image.jpg");
     }
 
 
     /** @test */
     function we_handle_crop_transformation_on_upload_from_front()
     {
-        Storage::disk('local')->put('toto', 'toto');
-
-        $formatter = new UploadFormatter;
-
-        $mock = \Mockery::mock(ImageManager::class);
-        $mock
-            ->shouldReceive('make')
-            ->once()
-            ->andReturn(new Class{
-                public function crop(){
-                    return true;
-                }
-                public function encode(){
-                    return true;
-                }
-                public function rotate(){
-                    return true;
-                }
-                public function width() {
-                        return true;
-                }
-                public function height() {
-                        return true;
-                }
-            });
-
-        $formatter->setImageManager($mock);
+        UploadedFile::fake()
+            ->image("image.jpg")
+            ->storeAs('/test', 'image.jpg', ['disk' => 'local']);
 
         $field = SharpFormUploadField::make("upload")
+            ->setStorageBasePath("/test")
+            ->setCropRatio("16:9")
             ->setStorageDisk("local");
 
-        $this->assertArrayContainsSubset([
-            "transformed" => true
-        ], $formatter->fromFront(
-            $field, "attribute", [
-                "name" => "toto",
-                "cropData" => [
-                    "height" => .8, "width" => .6, "x" => 0, "y" => .1, "rotate" => 1
-                ],
-                "uploaded" => false
-            ]
-        ));
+        $this->assertArrayContainsSubset(
+            ["transformed" => true],
+            (new UploadFormatter)->fromFront(
+                $field, 
+                "attribute", 
+                [
+                    "name" => "/test/image.jpg",
+                    "cropData" => [
+                        "height" => .8, "width" => .6, "x" => 0, "y" => .1, "rotate" => 1
+                    ],
+                    "uploaded" => false
+                ]
+            )
+        );
     }
 
-    /**
-     * @test
-     */
+    /** @test */
     function we_handle_crop_transformation_on_a_previously_upload_from_front()
     {
-        Storage::disk('local')->put('tmp/toto', 'toto');
-
-        $formatter = new UploadFormatter;
-
-        $mock = \Mockery::mock(ImageManager::class);
-        $mock
-            ->shouldReceive('make')
-            ->once()
-            ->andReturn(new Class{
-                public function crop(){
-                    return true;
-                }
-                public function encode(){
-                    return true;
-                }
-                public function rotate(){
-                    return true;
-                }
-                public function width() {
-                    return true;
-                }
-                public function height() {
-                    return true;
-                }
-            });
-
-        $formatter->setImageManager($mock);
+        UploadedFile::fake()
+            ->image("image.jpg", 600, 600)
+            ->storeAs('/data/test', 'image.jpg', ['disk' => 'local']);
 
         $field = SharpFormUploadField::make("upload")
-            ->setStorageDisk("local");
+            ->setStorageDisk("local")
+            ->setCropRatio("16:9")
+            ->setStorageBasePath("data/test");
 
-        $this->assertArrayContainsSubset([
-            "transformed" => true
-        ], $formatter->fromFront(
-            $field, "attribute", [
-                "name" => "toto",
-                "cropData" => [
-                    "height" => .8, "width" => .6, "x" => 0, "y" => .1, "rotate" => 1
-                ],
-                "uploaded" => true
-            ]
-        ));
+        $this->assertArrayContainsSubset(
+            ["transformed" => true], 
+            (new UploadFormatter)->fromFront(
+                $field, 
+                "attribute", [
+                    "name" => "/data/test/image.jpg",
+                    "cropData" => [
+                        "height" => .8, "width" => .6, "x" => 0, "y" => .1, "rotate" => 1
+                    ],
+                    "uploaded" => false
+                ]
+            )
+        );
     }
 
-    /**
-     * @test
-     */
-    public function we_should_optimize()
+    /** @test */
+    public function we_optimize_uploaded_images_if_configured()
     {
         Storage::disk('local')->put('tmp/toto', 'toto');
 
