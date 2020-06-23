949c98bf9d5acf78b8fb10185f03fe444604ed06
antoine <antoine.guingand@wanadoo.fr>
2020-06-23 14:35:27 +0200

    Improve saturn log alert

--- saturn/resources/views/sharp/_login-page-message.blade.php
+++ saturn/resources/views/sharp/_login-page-message.blade.php
@@ -2,11 +2,28 @@
 <div role="alert" class="SharpNotification SharpNotification--info">
     <div class="SharpNotification__details">
         <div class="SharpNotification__text-wrapper">
-            <p class="SharpNotification__subtitle">
+            <div class="SharpNotification__title mb-2">
                 Use these accounts to login:
+            </div>
+            <p class="SharpNotification__subtitle">
                 <ul>
-                    <li>- admin@example.com / secret</li>
-                    <li>- boss@example.com / secret <span style="font-style: italic">(has a few more permissions)</span></li>
+                    <li class="mb-2">
+                        <input
+                            class="SharpButton SharpButton--sm SharpButton--secondary"
+                            type="button"
+                            onclick="this.form.elements.login.value='admin@example.com';this.form.elements.password.value='secret'"
+                            value="Admin account"
+                        >
+                    </li>
+                    <li>
+                        <input
+                            class="SharpButton SharpButton--sm SharpButton--secondary"
+                            type="button"
+                            onclick="this.form.elements.login.value='boss@example.com';this.form.elements.password.value='secret'"
+                            value="Boss account"
+                        >
+                        <span style="font-style: italic">(has a few more permissions)</span>
+                    </li>
                 </ul>
             </p>
         </div>
