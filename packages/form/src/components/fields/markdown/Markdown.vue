b406194f83576e0e4252eab38a4ce1f32074c7ac
antoine <antoine.guingand@wanadoo.fr>
2020-12-16 13:51:46 +0100

    Handle paste / drop table in markdown

--- packages/form/src/components/fields/markdown/Markdown.vue
+++ packages/form/src/components/fields/markdown/Markdown.vue
@@ -22,6 +22,7 @@
 
     import localize from '../../../mixins/localize/editor';
     import { buttons } from './config';
+    import { handleMarkdownTables } from "./tables";
 
     const noop = ()=>{};
 
@@ -351,8 +352,7 @@
             },
 
             createSimpleMDE({ element, initialValue }) {
-                //console.log('initialValue',initialValue);
-                let simplemde = new SimpleMDE({
+                const simplemde = new SimpleMDE({
                     element,
                     initialValue,
                     placeholder: this.placeholder,
@@ -379,6 +379,8 @@
                 this.codemirrorOn(codemirror, 'cursorActivity', this.onCursorActivity, true);
                 this.codemirrorOn(codemirror, 'change', this.onChange);
                 this.codemirrorOn(codemirror, 'beforeChange',this.onBeforeChange);
+
+                handleMarkdownTables(codemirror);
             }
         },
         mounted() {
