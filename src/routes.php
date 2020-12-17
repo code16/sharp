1e76c05aa3d3c01bccfd11ea5d673761ee01dc9d
philippe <philippe@code16.fr>
2020-12-17 14:15:12 +0100

    Use classes in route file

--- src/routes.php
+++ src/routes.php
@@ -1,129 +1,115 @@
 <?php
 
-// API routes
+use Code16\Sharp\Http\Api\Commands\DashboardCommandController;
+use Code16\Sharp\Http\Api\Commands\EntityCommandController;
+use Code16\Sharp\Http\Api\Commands\EntityListInstanceCommandController;
+use Code16\Sharp\Http\Api\Commands\EntityListInstanceStateController;
+use Code16\Sharp\Http\Api\Commands\ShowInstanceCommandController;
+use Code16\Sharp\Http\Api\Commands\ShowInstanceStateController;
+use Code16\Sharp\Http\Api\DownloadController;
+use Code16\Sharp\Http\Api\EntityListController;
+use Code16\Sharp\Http\Api\FormController;
 use Code16\Sharp\Http\Api\FormUploadController;
+use Code16\Sharp\Http\Api\GlobalFilterController;
+use Code16\Sharp\Http\Api\ShowController;
 use Code16\Sharp\Http\DashboardController;
 use Code16\Sharp\Http\HomeController;
+use Code16\Sharp\Http\LangController;
 use Code16\Sharp\Http\ListController;
 use Code16\Sharp\Http\LoginController;
 use Code16\Sharp\Http\SingleShowController;
 use Code16\Sharp\Http\WebDispatchController;
 
+// API routes
 Route::group([
     'prefix' => '/' . sharp_base_url_segment() . '/api',
     'middleware' => ['sharp_web', 'sharp_api_errors', 'sharp_api_context', 'sharp_api_validation', 'sharp_locale'],
-    'namespace' => 'Code16\Sharp\Http\Api'
 ], function() {
 
-    Route::get("/dashboard/{dashboardKey}")
+    Route::get("/dashboard/{dashboardKey}", [\Code16\Sharp\Http\Api\DashboardController::class, "show"])
         ->name("code16.sharp.api.dashboard")
-        ->middleware(['sharp_api_append_breadcrumb'])
-        ->uses('DashboardController@show');
+        ->middleware(['sharp_api_append_breadcrumb']);
 
-    Route::get("/dashboard/{dashboardKey}/command/{commandKey}/data")
-        ->name("code16.sharp.api.dashboard.command.data")
-        ->uses('Commands\DashboardCommandController@show');
+    Route::get("/dashboard/{dashboardKey}/command/{commandKey}/data", [DashboardCommandController::class, "show"])
+        ->name("code16.sharp.api.dashboard.command.data");
 
-    Route::post("/dashboard/{dashboardKey}/command/{commandKey}")
-        ->name("code16.sharp.api.dashboard.command")
-        ->uses('Commands\DashboardCommandController@update');
+    Route::post("/dashboard/{dashboardKey}/command/{commandKey}", [DashboardCommandController::class, "update"])
+        ->name("code16.sharp.api.dashboard.command");
 
-    Route::get("/list/{entityKey}")
+    Route::get("/list/{entityKey}", [EntityListController::class, "show"])
         ->name("code16.sharp.api.list")
-        ->middleware(['sharp_api_append_list_authorizations', 'sharp_api_append_list_multiform', 'sharp_api_append_notifications', 'sharp_api_append_breadcrumb'])
-        ->uses('EntityListController@show');
+        ->middleware(['sharp_api_append_list_authorizations', 'sharp_api_append_list_multiform', 'sharp_api_append_notifications', 'sharp_api_append_breadcrumb']);
 
-    Route::post("/list/{entityKey}/reorder")
-        ->name("code16.sharp.api.list.reorder")
-        ->uses('EntityListController@update');
+    Route::post("/list/{entityKey}/reorder", [EntityListController::class, "update"])
+        ->name("code16.sharp.api.list.reorder");
 
-    Route::post("/list/{entityKey}/state/{instanceId}")
-        ->name("code16.sharp.api.list.state")
-        ->uses('Commands\EntityListInstanceStateController@update');
+    Route::post("/list/{entityKey}/state/{instanceId}", [EntityListInstanceStateController::class, "update"])
+        ->name("code16.sharp.api.list.state");
 
-    Route::post("/list/{entityKey}/command/{commandKey}")
-        ->name("code16.sharp.api.list.command.entity")
-        ->uses('Commands\EntityCommandController@update');
+    Route::post("/list/{entityKey}/command/{commandKey}", [EntityCommandController::class, "update"])
+        ->name("code16.sharp.api.list.command.entity");
 
-    Route::get("/list/{entityKey}/command/{commandKey}/data")
-        ->name("code16.sharp.api.list.command.entity.data")
-        ->uses('Commands\EntityCommandController@show');
+    Route::get("/list/{entityKey}/command/{commandKey}/data", [EntityCommandController::class, "show"])
+        ->name("code16.sharp.api.list.command.entity.data");
 
-    Route::post("/list/{entityKey}/command/{commandKey}/{instanceId}")
-        ->name("code16.sharp.api.list.command.instance")
-        ->uses('Commands\EntityListInstanceCommandController@update');
+    Route::post("/list/{entityKey}/command/{commandKey}/{instanceId}", [EntityListInstanceCommandController::class, "update"])
+        ->name("code16.sharp.api.list.command.instance");
 
-    Route::get("/list/{entityKey}/command/{commandKey}/{instanceId}/data")
-        ->name("code16.sharp.api.list.command.instance.data")
-        ->uses('Commands\EntityListInstanceCommandController@show');
+    Route::get("/list/{entityKey}/command/{commandKey}/{instanceId}/data", [EntityListInstanceCommandController::class, "show"])
+        ->name("code16.sharp.api.list.command.instance.data");
 
-    Route::get("/show/{entityKey}/{instanceId?}")
+    Route::get("/show/{entityKey}/{instanceId?}", [ShowController::class, "show"])
         ->name("code16.sharp.api.show.show")
-        ->middleware(['sharp_api_append_form_authorizations', 'sharp_api_append_breadcrumb'])
-        ->uses('ShowController@show');
+        ->middleware(['sharp_api_append_form_authorizations', 'sharp_api_append_breadcrumb']);
 
-    Route::get("/show/download/{fieldKey}/{entityKey}/{instanceId?}")
-        ->name("code16.sharp.api.show.download")
-        ->uses('DownloadController@show');
+    Route::get("/show/download/{fieldKey}/{entityKey}/{instanceId?}", [DownloadController::class, "show"])
+        ->name("code16.sharp.api.show.download");
 
-    Route::post("/show/{entityKey}/command/{commandKey}/{instanceId?}")
-        ->name("code16.sharp.api.show.command.instance")
-        ->uses('Commands\ShowInstanceCommandController@update');
+    Route::post("/show/{entityKey}/command/{commandKey}/{instanceId?}", [ShowInstanceCommandController::class, "update"])
+        ->name("code16.sharp.api.show.command.instance");
 
-    Route::get("/show/{entityKey}/command/{commandKey}/{instanceId}/data")
-        ->name("code16.sharp.api.show.command.instance.data")
-        ->uses('Commands\ShowInstanceCommandController@show');
+    Route::get("/show/{entityKey}/command/{commandKey}/{instanceId}/data", [ShowInstanceCommandController::class, "show"])
+        ->name("code16.sharp.api.show.command.instance.data");
 
     // Specific route for single shows, because /show/{entityKey}/command/{commandKey}/{instanceId?}/data
     // does not work since instanceId is optional but not the last segment.
-    Route::get("/show/{entityKey}/command/{commandKey}/data")
-        ->name("code16.sharp.api.show.command.singleInstance.data")
-        ->uses('Commands\ShowInstanceCommandController@show');
+    Route::get("/show/{entityKey}/command/{commandKey}/data", [ShowInstanceCommandController::class, "show"])
+        ->name("code16.sharp.api.show.command.singleInstance.data");
 
-    Route::post("/show/{entityKey}/state/{instanceId?}")
-        ->name("code16.sharp.api.show.state")
-        ->uses('Commands\ShowInstanceStateController@update');
+    Route::post("/show/{entityKey}/state/{instanceId?}", [ShowInstanceStateController::class, "update"])
+        ->name("code16.sharp.api.show.state");
 
-    Route::get("/form/{entityKey}")
+    Route::get("/form/{entityKey}", [FormController::class, "create"])
         ->name("code16.sharp.api.form.create")
-        ->middleware(['sharp_api_append_form_authorizations', 'sharp_api_append_breadcrumb'])
-        ->uses('FormController@create');
+        ->middleware(['sharp_api_append_form_authorizations', 'sharp_api_append_breadcrumb']);
 
-    Route::post("/form/{entityKey}")
-        ->name("code16.sharp.api.form.store")
-        ->uses('FormController@store');
+    Route::post("/form/{entityKey}", [FormController::class, "store"])
+        ->name("code16.sharp.api.form.store");
 
-    Route::get("/form/{entityKey}/{instanceId?}")
+    Route::get("/form/{entityKey}/{instanceId?}", [FormController::class, "edit"])
         ->name("code16.sharp.api.form.edit")
-        ->middleware(['sharp_api_append_form_authorizations', 'sharp_api_append_breadcrumb'])
-        ->uses('FormController@edit');
+        ->middleware(['sharp_api_append_form_authorizations', 'sharp_api_append_breadcrumb']);
 
-    Route::post("/form/{entityKey}/{instanceId?}")
-        ->name("code16.sharp.api.form.update")
-        ->uses('FormController@update');
+    Route::post("/form/{entityKey}/{instanceId?}", [FormController::class, "update"])
+        ->name("code16.sharp.api.form.update");
 
-    Route::delete("/form/{entityKey}/{instanceId?}")
-        ->name("code16.sharp.api.form.delete")
-        ->uses('FormController@delete');
+    Route::delete("/form/{entityKey}/{instanceId?}", [FormController::class, "delete"])
+        ->name("code16.sharp.api.form.delete");
 
-    Route::get("/form/download/{fieldKey}/{entityKey}/{instanceId?}")
-        ->name("code16.sharp.api.form.download")
-        ->uses('DownloadController@show');
+    Route::get("/form/download/{fieldKey}/{entityKey}/{instanceId?}", [DownloadController::class, "show"])
+        ->name("code16.sharp.api.form.download");
 
-    Route::get("/filters")
-        ->name("code16.sharp.api.filter.index")
-        ->uses('GlobalFilterController@index');
+    Route::get("/filters", [GlobalFilterController::class, "index"])
+        ->name("code16.sharp.api.filter.index");
 
-    Route::post("/filters/{filterKey}")
-        ->name("code16.sharp.api.filter.update")
-        ->uses('GlobalFilterController@update');
+    Route::post("/filters/{filterKey}", [GlobalFilterController::class, "update"])
+        ->name("code16.sharp.api.filter.update");
 });
 
 // Web routes
 Route::group([
     'prefix' => '/' . sharp_base_url_segment(),
     'middleware' => ['sharp_web', 'sharp_invalidate_cache'],
-    'namespace' => 'Code16\Sharp\Http'
 ], function() {
 
     Route::get('/login', [LoginController::class, "create"])
@@ -158,6 +144,5 @@ Route::group([
 });
 
 // Localization
-Route::get('/vendor/sharp/lang.js')
-    ->name('code16.sharp.assets.lang')
-    ->uses('Code16\Sharp\Http\LangController@index');
+Route::get('/vendor/sharp/lang.js', [LangController::class, "index"])
+    ->name('code16.sharp.assets.lang');
