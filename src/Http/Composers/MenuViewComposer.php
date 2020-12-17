e1e5099054a3191f64b945106532911905f65f42
philippe <philippe@code16.fr>
2020-12-17 17:55:38 +0100

    Fix current menu

--- src/Http/Composers/MenuViewComposer.php
+++ src/Http/Composers/MenuViewComposer.php
@@ -3,11 +3,19 @@
 namespace Code16\Sharp\Http\Composers;
 
 use Code16\Sharp\Http\Composers\Utils\MenuItem;
+use Code16\Sharp\Http\Context\CurrentSharpRequest;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Collection;
 
 class MenuViewComposer
 {
+    /** @var CurrentSharpRequest */
+    protected $currentSharpRequest;
+
+    public function __construct(CurrentSharpRequest $currentSharpRequest)
+    {
+        $this->currentSharpRequest = $currentSharpRequest;
+    }
 
     /**
      * Build the menu and bind it to the view.
@@ -24,12 +32,12 @@ class MenuViewComposer
                 $menuItems->push($menuItem);
             }
         }
-
+        
         $view->with('sharpMenu', (object)[
             "name" => config("sharp.name", "Sharp"),
             "user" => sharp_user()->{config("sharp.auth.display_attribute", "name")},
             "menuItems" => $menuItems,
-            "currentEntity" => (session("sharp_breadcrumb") ?? [])[0]["entity_key"] ?? null
+            "currentEntity" => $this->currentSharpRequest->breadcrumb()->first()->key ?? null
         ]);
 
         $view->with('hasGlobalFilters', sizeof(config('sharp.global_filters') ?? []) > 0);
