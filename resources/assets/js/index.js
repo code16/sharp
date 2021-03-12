2870442a96439608191e82bd7d88e3de044eeb41
antoine <antoine.guingand@wanadoo.fr>
2021-03-12 11:19:57 +0100

    Rmove useless exports

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -7,7 +7,7 @@ export * from './util/log';
 export * from './util/file';
 export * from './consts';
 export { showAlert, showConfirm } from './util/dialogs';
-export { getBackUrl, getDeleteBackUrl, entitiesMatch } from './util/breadcrumb';
+export { getBackUrl } from './util/breadcrumb';
 export { isCustomField, resolveCustomField } from './util/fields';
 export { handleNotifications } from './util/notifications';
 export { formUrl } from 'sharp-form/src/util/url';
