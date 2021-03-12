b5dee7d685f452382d0377c17df7f0c16ca84a95
antoine <antoine.guingand@wanadoo.fr>
2021-03-12 11:22:28 +0100

    restore entitiesMath function

--- resources/assets/js/util/breadcrumb.js
+++ resources/assets/js/util/breadcrumb.js
@@ -4,3 +4,10 @@ export function getBackUrl(breadcrumb) {
     const item = breadcrumb[breadcrumb.length - 2];
     return item ? item.url : null;
 }
+
+export function entitiesMatch(entityKeyA, entityKeyB) {
+    if(!entityKeyA || !entityKeyB) {
+        return false;
+    }
+    return entityKeyA.replace(/:(.*)/, '') === entityKeyB.replace(/:(.*)/, '');
+}
