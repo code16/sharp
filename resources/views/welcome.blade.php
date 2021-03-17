1bad821a0483a3ace673df8871dd6fc56cb03986
antoine <antoine.guingand@wanadoo.fr>
2021-03-17 20:09:18 +0100

    improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,13 @@
 @section("content")
 
     <x-sharp::page class="Sharp__dashboard-page">
-        <div class="SharpActionView Sharp__empty-view">
+        <div class="SharpActionBar__bar"></div>
+        <div class="SharpActionView vh-100">
             <div class="container h-100 d-flex justify-content-center align-items-center">
-                <h1>
+                <h1 class="text-muted">
                     @lang("sharp::menu.no-dashboard-message")
                 </h1>
             </div>
         </div>
     </x-sharp::page>
-
 @endsection
