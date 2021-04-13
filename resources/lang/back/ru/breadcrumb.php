3c7f8ac8042a13a70e9c2b59c27bb698ba286cc8
Insolita <Insolita@users.noreply.github.com>
2021-04-13 22:54:00 +0800

    Add translation for breadcrumbs

---
+++ resources/lang/back/ru/breadcrumb.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'dashboard' => 'Панель',
+    'entityList' => 'Список',
+    'show' => ':entity',
+    'form' => [
+        'edit' => 'Редактирование',
+        'edit_entity' => 'Редактирование “:entity”', // for sub entities: forms displayed from an EEL
+        'create' => 'Создание “:entity”'
+    ],
+];
