1d48ec52bae5e35a7e4fdd3ec9aa67084be43375
antoine <antoine.guingand@wanadoo.fr>
2021-06-23 17:48:45 +0200

    refactor SharpEditor component

---
+++ packages/form/src/components/fields/editor/Editor.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="editor" :class="classes" :style="style">
+        <div class="card">
+            <div class="card-header">
+                <template v-if="editor">
+                    <MenuBar :id="uniqueIdentifier" :editor="editor" :toolbar="toolbar" />
+                </template>
+            </div>
+
+            <EditorContent :editor="editor" />
+
+            <template v-if="editor">
+                <BubbleMenu
+                    :id="uniqueIdentifier"
+                    :editor="editor"
+                    :toolbar="toolbar"
+                    :ignored-extensions="bubbleMenuIgnoredExtensions"
+                />
+                <template v-if="hasUpload">
+                    <UploadFileInput :editor="editor"/>
+                </template>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { EditorContent } from '@tiptap/vue-2';
+    import HorizontalRule from "@tiptap/extension-horizontal-rule";
+    import { Upload } from "./extensions/upload/upload";
+    import UploadFileInput from "./extensions/upload/UploadFileInput";
+    import MenuBar from "./toolbar/MenuBar";
+    import BubbleMenu from "./BubbleMenu";
+
+    export default {
+        components: {
+            EditorContent,
+            MenuBar,
+            UploadFileInput,
+            BubbleMenu,
+        },
+        props: {
+            editor: Object,
+            uniqueIdentifier: String,
+            toolbar: Array,
+            height: {
+                type: Number,
+                default: 300
+            },
+            readOnly: Boolean,
+        },
+        computed: {
+            classes() {
+                return {
+                    'editor--disabled': this.readOnly,
+                }
+            },
+            style() {
+                return {
+                    '--height': `${this.height}px`,
+                }
+            },
+            bubbleMenuIgnoredExtensions() {
+                return [
+                    Upload,
+                    HorizontalRule,
+                ]
+            },
+            hasUpload() {
+                return this.editor.options.extensions?.find(extension => extension.name === Upload.name);
+            },
+        },
+        async mounted() {
+            await this.$nextTick();
+            this.editor.view.dom.classList.add(
+                'card-body',
+                'form-control',
+                'editor__content',
+            );
+        },
+    }
+</script>
--- packages/form/src/components/fields/markdown/Markdown.vue
+++ packages/form/src/components/fields/markdown/Markdown.vue
@@ -1,52 +1,26 @@
 <template>
-    <div class="SharpMarkdown editor" :style="style">
-        <div class="card">
-            <div class="card-header">
-                <template v-if="editor">
-                    <MenuBar :id="uniqueIdentifier" :editor="editor" :toolbar="toolbar" />
-                </template>
-            </div>
-            <EditorContent :editor="editor" />
-
-            <template v-if="editor">
-                <BubbleMenu
-                    :id="uniqueIdentifier"
-                    :editor="editor"
-                    :toolbar="toolbar"
-                    :ignored-extensions="bubbleMenuIgnoredExtensions"
-                />
-                <UploadFileInput :editor="editor"/>
-            </template>
-        </div>
-    </div>
+    <SharpEditor
+        class="SharpMarkdown"
+        :editor="editor"
+        :toolbar="toolbar"
+        :height="height"
+        :unique-identifier="uniqueIdentifier"
+        :read-only="readOnly"
+    >
+    </SharpEditor>
 </template>
 
 <script>
-    import { createMarkdownEditor, createMarkdownExtension } from 'tiptap-markdown';
-    import { Editor, EditorContent } from '@tiptap/vue-2';
-    import StarterKit from '@tiptap/starter-kit';
-    import Table from '@tiptap/extension-table';
-    import TableRow from '@tiptap/extension-table-row';
-    import TableCell from '@tiptap/extension-table-cell';
-    import TableHeader from '@tiptap/extension-table-header';
-    import Image from '@tiptap/extension-image';
-    import Link from '@tiptap/extension-link';
-    import HorizontalRule from '@tiptap/extension-horizontal-rule';
-    import MenuBar from "./toolbar/MenuBar";
+    import { createMarkdownEditor } from 'tiptap-markdown';
+    import { Editor } from '@tiptap/vue-2';
+    import SharpEditor from '../editor/Editor';
+    import { getDefaultExtensions, getUploadExtension } from "../editor";
     import localize from '../../../mixins/localize/editor';
-    import { Upload } from "./extensions/upload/upload";
-    import { TrailingNode } from "./extensions/trailing-node";
-    import UploadFileInput from "./extensions/upload/UploadFileInput";
-    import BubbleMenu from "./BubbleMenu";
-    import { filesEquals } from "../../../util/upload";
 
     export default {
         mixins: [ localize({ textProp:'text' }) ],
         components: {
-            EditorContent,
-            BubbleMenu,
-            MenuBar,
-            UploadFileInput,
+            SharpEditor,
         },
         props: {
             id: String,
@@ -56,10 +30,7 @@
             },
             placeholder: String,
             toolbar: Array,
-            height: {
-                type: Number,
-                default: 300
-            },
+            height: Number,
             innerComponents: Object,
 
             readOnly: Boolean,
@@ -69,23 +40,11 @@
         data() {
             return {
                 editor: null,
-                currentFileId: 0,
             }
         },
         computed: {
-            style() {
-                return {
-                    '--height': `${this.height}px`,
-                }
-            },
             hasUpload() {
                 return !!this.innerComponents?.upload;
-            },
-            bubbleMenuIgnoredExtensions() {
-                return [
-                    Upload,
-                    HorizontalRule,
-                ]
             },
         },
         methods: {
@@ -94,83 +53,35 @@
                 const content = this.editor.getMarkdown();
                 this.$emit('input', this.localizedValue(content));
             },
-            getUploadExtension() {
-                return Upload.configure({
-                    fieldProps: {
-                        ...this.innerComponents.upload,
-                        uniqueIdentifier: this.uniqueIdentifier,
-                        fieldConfigIdentifier: this.fieldConfigIdentifier,
-                    },
-                    findFile: attrs => {
-                        return this.value.files?.find(file => filesEquals(attrs, file));
-                    },
-                    onSuccess: (value) => {
-                        this.$emit('input', {
-                            ...this.value,
-                            files: [...(this.value?.files ?? []), value],
-                        });
-                    },
-                    onRemove: (value) => {
-                        this.$emit('input', {
-                            ...this.value,
-                            files: this.value.files?.filter(file => !filesEquals(file, value)),
-                        });
-                    },
-                    onUpdate: (value) => {
-                        this.$emit('input', {
-                            ...this.value,
-                            files: this.value.files?.map(file => filesEquals(file, value) ? value : file),
-                        });
-                    },
-                });
-            },
+
             createEditor() {
                 const MarkdownEditor = createMarkdownEditor(Editor);
-                const markdownExtensions = [];
                 const extensions = [
-                    StarterKit.configure({
-                        horizontalRule: false,
+                    ...getDefaultExtensions({
+                        placeholder: this.placeholder,
                     }),
-                    Table,
-                    TableRow,
-                    TableHeader,
-                    TableCell,
-                    Image,
-                    HorizontalRule.extend({
-                        selectable: false,
-                    }),
-                    Link.configure({
-                        openOnClick: false,
-                    }),
-                    TrailingNode,
                 ]
 
                 if(this.hasUpload) {
-                    extensions.push(this.getUploadExtension());
+                    const Upload = getUploadExtension.call(this, {
+                        fieldProps: this.innerComponents.upload,
+                    });
+                    extensions.push(Upload);
                 }
 
-                this.editor = new MarkdownEditor({
+                return new MarkdownEditor({
                     extensions,
                     content: this.localizedText,
                     onUpdate: this.handleUpdate,
-                    markdown: {
-                        extensions: markdownExtensions,
-                    }
+                    editable: !this.readOnly,
                 });
-
-                this.editor.view.dom.classList.add(
-                    'card-body',
-                    'form-control',
-                    'editor__content',
-                    'SharpMarkdown__form-control',
-                );
             },
         },
         mounted() {
-            this.createEditor();
+            this.editor = this.createEditor();
         },
         beforeDestroy() {
-            this.editor.destroy()
+            this.editor.destroy();
         },
     }
 </script>
