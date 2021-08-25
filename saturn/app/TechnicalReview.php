03e8e9bbc6cbe471d63dcd8665e9c3ab2f0a7822
Klaas Poortinga <poortinga.k@gmail.com>
2021-08-25 19:04:46 +0200

    Add example of form list with translatable wysiwyg editor

--- saturn/app/TechnicalReview.php
+++ saturn/app/TechnicalReview.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class TechnicalReview extends Model
 {
+    use HasTranslations;
+
+    public $translatable = ['report'];
+
     protected $dates = [
         "created_at", "updated_at", "starts_at"
     ];
