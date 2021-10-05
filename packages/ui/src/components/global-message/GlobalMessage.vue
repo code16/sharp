d6c517dde3762e36bbe87597fbf62ed562b26927
antoine <antoine.guingand@wanadoo.fr>
2021-10-05 19:32:54 +0200

    handle global message component

---
+++ packages/ui/src/components/global-message/GlobalMessage.vue
@@ -0,0 +1,39 @@
+<template>
+    <GlobalMessageLayout class="SharpGlobalMessage" :options="options">
+        <template v-if="fieldOptions">
+            <TemplateRenderer
+                name="GlobalMessage"
+                :template-data="value"
+                :template="fieldOptions.template"
+            ></TemplateRenderer>
+        </template>
+    </GlobalMessageLayout>
+</template>
+
+<script>
+    import { TemplateRenderer } from "sharp/components";
+    import GlobalMessageLayout from "./GlobalMessageLayout";
+
+    export default {
+        components: {
+            GlobalMessageLayout,
+            TemplateRenderer,
+        },
+        props: {
+            options: Object,
+            fields: Object,
+            data: Object,
+        },
+        computed: {
+            fieldKey() {
+                return this.options.fieldKey;
+            },
+            fieldOptions() {
+                return this.fields?.[this.fieldKey];
+            },
+            value() {
+                return this.data?.[this.fieldKey];
+            },
+        },
+    }
+</script>
---
+++ packages/ui/src/components/global-message/GlobalMessageLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <template v-if="options.alertLevel">
+            <div class="alert mb-0" :class="alertClasses">
+                <slot />
+            </div>
+        </template>
+        <template v-else>
+            <slot />
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            options: Object,
+        },
+        computed: {
+            alertClasses() {
+                return [
+                    `alert-${this.options.alertLevel}`,
+                ];
+            },
+        },
+    }
+</script>
