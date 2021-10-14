f966fc6b07e9a12b670ba2f92f963c656a679ef6
philippe <philippe@code16.fr>
2021-10-14 16:48:37 +0200

    Fix Saturn Spaceship update

--- saturn/tests/Feature/SpaceshipSharpFormTest.php
+++ saturn/tests/Feature/SpaceshipSharpFormTest.php
@@ -124,15 +124,21 @@ class SpaceshipSharpFormTest extends TestCase
     {
         $this->loginAsSharpUser(factory(User::class)->create(["group" => "sharp"]));
 
-        $this->storeSharpForm("spaceship", array_merge(factory(Spaceship::class)->make()->toArray(), [
-            "name" => ["fr" => "test_create"],
-            "capacity" => 10
-        ]))->assertStatus(200);
+        $this
+            ->storeSharpForm("spaceship", 
+                array_merge(
+                    factory(Spaceship::class)->make()->toArray(), 
+                    [
+                        "name" => ["fr" => "test_create"],
+                        "capacity" => 10
+                    ]
+                )
+            )
+            ->assertOk();
 
         $this->assertDatabaseHas("spaceships", [
             "name" => json_encode(["fr" => "test_create"]),
             "capacity" => 10000
         ]);
     }
-
 }
