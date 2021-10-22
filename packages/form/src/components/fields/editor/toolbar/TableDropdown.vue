b21728446291f8ba7ad875c675d17f983427ba2f
antoine <antoine.guingand@wanadoo.fr>
2021-10-22 16:28:44 +0200

    improve tables

--- packages/form/src/components/fields/editor/toolbar/TableDropdown.vue
+++ packages/form/src/components/fields/editor/toolbar/TableDropdown.vue
@@ -11,27 +11,70 @@
         </template>
 
         <template v-slot:default="{ hide }">
-            <template v-if="active">
-
-            </template>
-            <template v-else>
-                <b-dropdown-item @click="handleInsertClicked">
-                    Insert table
-                </b-dropdown-item>
-            </template>
+            <DropdownItem
+                :disabled="editor.isActive('table')"
+                @click="handleInsertClicked"
+            >
+                Insert table
+            </DropdownItem>
+            <DropdownSeparator />
+            <DropdownItem
+                :disabled="!editor.can().addRowBefore()"
+                @click="command(() => editor.chain().addRowBefore().run())"
+            >
+                Insert row above
+            </DropdownItem>
+            <DropdownItem
+                :disabled="!editor.can().addRowAfter()"
+                @click="command(() => editor.chain().addRowAfter().run())"
+            >
+                Insert row below
+            </DropdownItem>
+            <DropdownItem
+                :disabled="!editor.can().deleteRow()"
+                @click="command(() => editor.chain().deleteRow().run())"
+            >
+                Remove row
+            </DropdownItem>
+            <DropdownSeparator />
+            <DropdownItem
+                :disabled="!editor.can().addColumnBefore()"
+                @click="command(() => editor.chain().addColumnBefore().run())"
+            >
+                Insert column to the left
+            </DropdownItem>
+            <DropdownItem
+                :disabled="!editor.can().addColumnAfter()"
+                @click="command(() => editor.chain().addColumnAfter().run())"
+            >
+                Insert column to the right
+            </DropdownItem>
+            <DropdownItem
+                :disabled="!editor.can().deleteColumn()"
+                @click="command(() => editor.chain().deleteColumn().run())"
+            >
+                Remove column
+            </DropdownItem>
+            <DropdownSeparator />
+            <DropdownItem
+                :disabled="!editor.can().deleteTable()"
+                @click="command(() => editor.chain().deleteTable().run())"
+            >
+                Remove table
+            </DropdownItem>
         </template>
     </Dropdown>
 </template>
 
 <script>
-    import { BDropdownItem } from 'bootstrap-vue';
-    import { Button, Dropdown } from "sharp-ui";
+    import { Button, Dropdown, DropdownItem, DropdownSeparator } from "sharp-ui";
 
     export default {
         components: {
             Button,
             Dropdown,
-            BDropdownItem,
+            DropdownItem,
+            DropdownSeparator,
         },
         props: {
             id: String,
@@ -44,11 +87,19 @@
         },
         methods: {
             handleInsertClicked() {
-                this.editor.commands.insertTable();
-                setTimeout(() => {
-                    this.editor.commands.focus();
-                }, 0);
+                this.editor.chain()
+                    .focus()
+                    .insertTable()
+                    .run();
             },
+            command(run) {
+                run();
+                setTimeout(() => {
+                    this.editor.chain()
+                        .focus()
+                        .run();
+                }, 0);
+            }
         }
     }
 </script>
