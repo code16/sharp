2ffb0bda11f907e5eb91b328848962c3ffe680e4
antoine <antoine.guingand@wanadoo.fr>
2021-10-28 16:27:34 +0200

    disable paste / input rules

--- packages/form/src/components/fields/editor/extensions/index.js
+++ packages/form/src/components/fields/editor/extensions/index.js
@@ -4,17 +4,19 @@ import Image from "@tiptap/extension-image";
 import HorizontalRule from "@tiptap/extension-horizontal-rule";
 import Placeholder from "@tiptap/extension-placeholder";
 import StarterKit from "@tiptap/starter-kit";
-import { TrailingNode } from "./trailing-node";
-import { Iframe } from "./iframe";
-import { getAllowedHeadingLevels, toolbarHasButton } from "../util";
 import Table from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
 import TableHeader from "@tiptap/extension-table-header";
 import TableCell from "@tiptap/extension-table-cell";
+import { getExtensionField, getSchema } from "@tiptap/core";
 import { Selected } from "./selected";
 import { Html } from "./html";
-import { getSchema } from "@tiptap/core";
+import { TrailingNode } from "./trailing-node";
+import { Iframe } from "./iframe";
 import { Paste } from "./paste";
+import { getAllowedHeadingLevels, toolbarHasButton } from "../util";
+import { disablePasteRules } from "../util/extensions";
+
 
 function getHeadingExtension(toolbar) {
     if(!toolbar) {
@@ -85,30 +87,35 @@ function getPasteExtension(toolbar) {
     });
 }
 
-function getToolbarExtensions(toolbar) {
+function getStarterKitExtensions(toolbar) {
     const bulletList = toolbarHasButton(toolbar, 'bullet-list');
     const orderedList = toolbarHasButton(toolbar, 'ordered-list');
+    const starterKit = StarterKit.configure({
+        blockquote: toolbarHasButton(toolbar, 'blockquote'),
+        bold: toolbarHasButton(toolbar, 'bold'),
+        bulletList,
+        code: toolbarHasButton(toolbar, 'code'),
+        codeBlock: false,
+        document: true,
+        dropcursor: true,
+        gapcursor: true,
+        hardBreak: true,
+        heading: false,
+        history: true,
+        horizontalRule: false,
+        italic: toolbarHasButton(toolbar, 'italic'),
+        listItem: bulletList || orderedList,
+        orderedList,
+        paragraph: true,
+        strike: false,
+        text: true,
+    });
+    return getExtensionField(starterKit, 'addExtensions', starterKit)();
+}
+
+function getToolbarExtensions(toolbar) {
     const extensions = [
-        StarterKit.configure({
-            blockquote: toolbarHasButton(toolbar, 'blockquote'),
-            bold: toolbarHasButton(toolbar, 'bold'),
-            bulletList,
-            code: toolbarHasButton(toolbar, 'code'),
-            codeBlock: false,
-            document: true,
-            dropcursor: true,
-            gapcursor: true,
-            hardBreak: true,
-            heading: false,
-            history: true,
-            horizontalRule: false,
-            italic: toolbarHasButton(toolbar, 'italic'),
-            listItem: bulletList || orderedList,
-            orderedList,
-            paragraph: true,
-            strike: false,
-            text: true,
-        }),
+        getStarterKitExtensions(toolbar),
         getHeadingExtension(toolbar),
         getLinkExtension(toolbar),
         getImageExtension(toolbar),
@@ -116,9 +123,11 @@ function getToolbarExtensions(toolbar) {
         getTableExtensions(toolbar),
         getIframeExtension(toolbar),
     ];
+
     return extensions
         .flat()
-        .filter(extension => !!extension);
+        .filter(extension => !!extension)
+        .map(extension => disablePasteRules(extension));
 }
 
 export function getDefaultExtensions({ placeholder, toolbar } = {}) {
--- packages/form/src/components/fields/editor/index.js
+++ packages/form/src/components/fields/editor/index.js
@@ -2,6 +2,7 @@
 
 export const defaultEditorOptions = {
     injectCSS: false,
+    enableInputRules: false,
 }
 
 export * from './extensions';
