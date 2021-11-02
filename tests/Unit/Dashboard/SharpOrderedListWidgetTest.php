e282969c0caf0ca32d7662593f2620f2ea7b215f
philippe <philippe@code16.fr>
2021-11-02 13:41:50 +0100

    Add html attribute

---
+++ tests/Unit/Dashboard/SharpOrderedListWidgetTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Code16\Sharp\Tests\Unit\Dashboard;
+
+use Code16\Sharp\Dashboard\Widgets\SharpOrderedListWidget;
+use Code16\Sharp\Tests\SharpTestCase;
+
+class SharpOrderedListWidgetTest extends SharpTestCase
+{
+    /** @test */
+    function we_can_define_html_attribute()
+    {
+        $widget = SharpOrderedListWidget::make("name")
+            ->setHtml();
+
+        $this->assertArraySubset(
+            ["html" => true],
+            $widget->toArray()
+        );
+    }
+}
