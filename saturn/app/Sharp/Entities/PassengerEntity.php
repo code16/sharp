7a8645de411062eb977afe3c09d51c9f2c74e005
philippe <philippe@code16.fr>
2021-12-24 10:06:15 +0100

    Doc and saturn fix

--- saturn/app/Sharp/Entities/PassengerEntity.php
+++ saturn/app/Sharp/Entities/PassengerEntity.php
@@ -4,13 +4,11 @@ namespace App\Sharp\Entities;
 
 use App\Sharp\PassengerSharpForm;
 use App\Sharp\PassengerSharpList;
-use App\Sharp\PilotSharpShow;
 use Code16\Sharp\Utils\Entities\SharpEntity;
 
 class PassengerEntity extends SharpEntity
 {
     protected ?string $list = PassengerSharpList::class;
     protected ?string $form = PassengerSharpForm::class;
-    protected ?string $show = PilotSharpShow::class;
     protected string $label = "Passenger";
 }
