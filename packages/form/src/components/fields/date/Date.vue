f436961caaf137992a6b93fa0b76bf7c8830555b
antoine <antoine.guingand@wanadoo.fr>
2022-02-28 16:31:04 +0100

    migrate to v-calendar WIP

--- packages/form/src/components/fields/date/Date.vue
+++ packages/form/src/components/fields/date/Date.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="SharpDate" :class="{'SharpDate--open':showPicker}">
         <div class="SharpDate__input-wrapper position-relative">
-            <div class="input-group" :class="{ 'input-group--clearable': hasClearButton }" ref="inputGroup">
-                <button class="input-group-text btn SharpDate__prepend"
-                    @pointerdown="handlePrependButtonPointerDown"
-                    @click="handlePrependButtonClicked"
-                >
-                    <svg class="align-middle" width="1.25em" height="1.25em" viewBox="0 0 32 32" style="fill:currentColor">
-                        <path d="M26,4h-4V2h-2v2h-8V2h-2v2H6C4.9,4,4,4.9,4,6v20c0,1.1,0.9,2,2,2h20c1.1,0,2-0.9,2-2V6C28,4.9,27.1,4,26,4z M26,26H6V12h20  V26z M26,10H6V6h4v2h2V6h8v2h2V6h4V10z"/>
-                    </svg>
-                </button>
-                <template v-if="displayFormat">
+            <v-date-picker
+                :mode="mode"
+                :value="pickerValue"
+                :valid-hours="validHours"
+                :minute-increment="stepTime"
+                is-24-hr
+                @input="handleDateChanged"
+                v-slot="{ inputEvents, togglePopover }"
+            >
+                <div class="input-group" :class="{ 'input-group--clearable': hasClearButton }" ref="inputGroup">
+                    <button class="input-group-text btn SharpDate__prepend"
+                        @click="togglePopover"
+                    >
+                        <svg class="align-middle" width="1.25em" height="1.25em" viewBox="0 0 32 32" style="fill:currentColor">
+                            <path d="M26,4h-4V2h-2v2h-8V2h-2v2H6C4.9,4,4,4.9,4,6v20c0,1.1,0.9,2,2,2h20c1.1,0,2-0.9,2-2V6C28,4.9,27.1,4,26,4z M26,26H6V12h20  V26z M26,10H6V6h4v2h2V6h8v2h2V6h4V10z"/>
+                        </svg>
+                    </button>
+
                     <input
                         :id="id"
                         class="form-control clearable SharpDate__input"
@@ -23,85 +31,28 @@
                         @blur="handleBlur"
                         @keydown.up.prevent="increase"
                         @keydown.down.prevent="decrease"
+                        v-on="inputEvents"
                         ref="input"
                     >
-                </template>
-                <template v-else>
-                    <template v-if="hasDate">
-                        <input
-                            :id="id"
-                            class="form-control SharpDate__date-input"
-                            :value="dateInputValue"
-                            type="date"
-                            autocomplete="off"
-                            @input="handleDateInput"
-                        >
-                    </template>
-                    <template v-if="hasTime">
-                        <input
-                            :id="!hasDate ? id : null"
-                            class="form-control SharpDate__date-input"
-                            :value="timeInputValue"
-                            type="time"
-                            autocomplete="off"
-                            @input="handleTimeInput"
-                        >
-                    </template>
-                </template>
 
-                <template v-if="hasClearButton">
-                    <ClearButton @click="clear" ref="clearButton" />
-                </template>
-            </div>
-        </div>
-        <template v-if="displayFormat">
-            <b-popover
-                :target="popoverTarget"
-                :show.sync="showPicker"
-                :boundary="popoverBoundary"
-                no-fade
-                triggers="focus"
-                placement="bottom"
-            >
-                <div class="SharpDate__picker position-static">
-                    <template v-if="hasDate">
-                        <DatePicker
-                            class="SharpDate__date"
-                            :language="language"
-                            :monday-first="mondayFirst"
-                            inline
-                            :value="dateObject"
-                            @selected="handleDateSelect"
-                            ref="datepicker"
-                        />
-                    </template>
-                    <template v-if="hasTime">
-                        <TimePicker
-                            class="SharpDate__time"
-                            :value="timeObject"
-                            :active="showPicker"
-                            :format="displayFormat"
-                            :minute-interval="stepTime"
-                            :min="minTime" :max="maxTime"
-                            @change="handleTimeSelect"
-                            ref="timepicker"
-                        />
+                    <template v-if="hasClearButton">
+                        <ClearButton @click="clear" ref="clearButton" />
                     </template>
                 </div>
-            </b-popover>
-        </template>
+            </v-date-picker>
+        </div>
     </div>
 </template>
 
 <script>
     import moment from 'moment';
     import { BPopover } from 'bootstrap-vue';
-
     import { lang } from 'sharp';
     import { Localization } from 'sharp/mixins';
     import { ClearButton } from "sharp-ui";
     import DatePicker from './Datepicker';
     import TimePicker from './Timepicker';
+    import { DatePicker as VDatePicker } from 'v-calendar';
 
 
     export default {
@@ -111,6 +62,7 @@
             TimePicker,
             BPopover,
             ClearButton,
+            VDatePicker,
         },
 
         inject:['$field'],
@@ -153,17 +105,24 @@
                     ? 'HH:mm'
                     : null;
             },
-            dateObject() {
+            mode() {
+                if(this.hasDate && this.hasTime) {
+                    return 'dateTime';
+                }
+                if(this.hasTime) {
+                    return 'time';
+                }
+                return 'date';
+            },
+            validHours() {
+                return {
+                    min: this.minTime ? parseInt(this.minTime) : null,
+                    max: this.maxTime ? parseInt(this.maxTime) : null,
+                }
+            },
+            pickerValue() {
                 return this.value
                     ? moment(this.value, this.format).toDate()
-                    : null;
-            },
-            timeObject() {
-                return this.value
-                    ? {
-                        HH: moment(this.value, this.format).format('HH'),
-                        mm: moment(this.value, this.format).format('mm')
-                    }
                     : null;
             },
             inputValue() {
@@ -177,70 +136,24 @@
             popoverBoundary() {
                 return document.querySelector('[data-popover-boundary]');
             },
-            timeInputValue() {
-                return this.value
-                    ? moment(this.value).format('HH:mm')
-                    : '';
-            },
-            dateInputValue() {
-                return this.value
-                    ? moment(this.value).format('YYYY-MM-DD')
-                    : '';
-            },
             hasClearButton() {
-                return !!this.displayFormat && !!this.value;
+                return !!this.value;
             },
         },
         methods: {
             popoverTarget() {
                 return this.$refs.inputGroup;
             },
-
             getMoment() {
                 return this.value
                     ? moment(this.value, this.format)
                     : moment();
             },
-            handleDateInput(e) {
-                if(!e.target.valueAsDate) {
-                    return;
-                }
-                // todo
+            handleDateChanged(date) {
                 this.$emit('input', date);
-            },
-            handleTimeInput(e) {
-                if(!this.hasDate) {
-                    return e.target.value;
-                }
-                if(!e.target.valueAsDate) {
-                    return;
-                }
-                // todo
-                this.$emit('input', date);
-            },
-            handleDateSelect(date) {
-                let newMoment = this.getMoment();
-                newMoment.set({
-                    year: date.getFullYear(),
-                    month: date.getMonth(),
-                    date: date.getDate()
-                });
-                this.$emit('input', newMoment.toDate());
-            },
-            handleTimeSelect({ data }) {
-                let newMoment = this.getMoment();
-                newMoment.set({
-                    hour: data.HH,
-                    minute: data.mm,
-                    second: data.ss,
-                });
-                if(this.getMoment().format('HH:mm') === newMoment.format('HH:mm')) {
-                    return;
-                }
-                this.$emit('input', newMoment.toDate());
             },
             handleInput(e) {
-                let m = moment(e.target.value, this.displayFormat, true);
+                const m = moment(e.target.value, this.displayFormat, true);
                 this.localInputValue = e.target.value;
                 this.showPicker = false;
                 if(!m.isValid()) {
