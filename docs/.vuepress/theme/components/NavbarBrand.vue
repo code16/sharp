f720f0003be4988187041ebcedd01b4885b754a7
antoine <antoine.guingand@wanadoo.fr>
2022-03-04 19:00:27 +0100

    use classic link for logo

--- docs/.vuepress/theme/components/NavbarBrand.vue
+++ docs/.vuepress/theme/components/NavbarBrand.vue
@@ -1,18 +1,20 @@
 <template>
-    <RouterLink :to="url" style="color: inherit">
-        <div class="row align-items-center gx-3">
-            <div class="col">
-                <Logo class="logo" width="120" />
+    <RouterLink :to="url" custom v-slot="{ href }">
+        <a :href="href" style="color: inherit">
+            <div class="row align-items-center gx-3">
+                <div class="col">
+                    <Logo class="logo" width="120" />
+                </div>
+                <div class="col-auto" :class="{'visually-hidden': !versionTitle}">
+                    <span class="site-name">
+                        <span class="visually-hidden">Sharp</span>
+                        <template v-if="versionTitle">
+                            <span class="version">{{ versionTitle }}</span>
+                        </template>
+                    </span>
+                </div>
             </div>
-            <div class="col-auto" :class="{'visually-hidden': !versionTitle}">
-                <span class="site-name">
-                    <span class="visually-hidden">Sharp</span>
-                    <template v-if="versionTitle">
-                        <span class="version">{{ versionTitle }}</span>
-                    </template>
-                </span>
-            </div>
-        </div>
+        </a>
     </RouterLink>
 </template>
 
@@ -21,8 +23,9 @@
     import { useRouteLocale, useSiteLocaleData } from "@vuepress/client";
     import Logo from '../../public/logo.svg';
 
-    const siteLocale = useSiteLocaleData();
     const routeLocale = useRouteLocale();
+    const siteLocale = useSiteLocaleData();
+
     const versionTitle = computed(() =>
         siteLocale.value.title?.match(/(Sharp )(.+)/i)?.[2]
     );
