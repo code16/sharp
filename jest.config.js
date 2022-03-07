1e88c07bfdbb18c3d79f3ee9793a3a4fe440a5ef
antoine <antoine.guingand@wanadoo.fr>
2022-03-07 19:27:06 +0100

    Move to jest.config.js file

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+    moduleFileExtensions: [
+        "js",
+        "vue"
+    ],
+    transform: {
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.vue$": "@vue/vue2-jest"
+    },
+    snapshotSerializers: [
+        "<rootDir>/packages/test-utils/src/htmlSnapshotBeautifier.js"
+    ],
+    moduleNameMapper: {
+        "^vue$": "vue/dist/vue.common.js",
+        "^sharp((/(.*))|$)": "<rootDir>/resources/assets/js$1",
+        "^babel-runtime/core-js/(.+)": "core-js/stable/$1", // for vue-clip
+        "\\.(css|less)$": "<rootDir>/packages/test-utils/src/setup/styleMock.js"
+    },
+    transformIgnorePatterns: [
+        "node_modules/(?!(vue2-google-maps|bootstrap-vue|vue-clip|vue2-timepicker))"
+    ],
+    testMatch: [
+        "**/__tests__/**/?(*.)(spec|test).js?(x)"
+    ],
+    testEnvironment: "jsdom",
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "/legacy/"
+    ],
+    verbose: true,
+    setupFiles: [
+        "<rootDir>/packages/test-utils/src/setup/mockument.js",
+        "<rootDir>/packages/test-utils/src/setup/mockBootstrapVue.js",
+        "<rootDir>/packages/test-utils/src/setup/setupVue.js",
+        "<rootDir>/resources/assets/js/polyfill.js"
+    ]
+};
+
+module.exports = config;
