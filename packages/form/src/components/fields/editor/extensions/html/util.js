ba1912d36d49093f06a0e24f7f636a581a847109
antoine <antoine.guingand@wanadoo.fr>
2022-03-21 16:39:19 +0100

    Unwrap embeds if inside raw html node

--- packages/form/src/components/fields/editor/extensions/html/util.js
+++ packages/form/src/components/fields/editor/extensions/html/util.js
@@ -22,6 +22,15 @@ export function setupContent(content, schema) {
 
 export function setupContentDOM(dom, schema) {
     [...dom.children].forEach(node => {
+        // in some cases we want to extract the html content if known (e.g. embeds)
+        if(node.hasAttribute('data-html-content') && node.children.length === 1) {
+            const childFragment = prosemirrorParse(node.firstElementChild, schema);
+            if(childFragment.firstChild?.type.name.startsWith('embed:')) {
+                node.parentElement.replaceChild(node.firstElementChild, node);
+            }
+            return;
+        }
+
         const parsed = prosemirrorParse(node, schema);
         if(!parsed.size) {
             const div = document.createElement('div');
