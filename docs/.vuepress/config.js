9685543fcdd4e2d36b9bdb0f421ad417e20ac836
antoine <antoine.guingand@wanadoo.fr>
2022-08-10 18:08:36 +0200

    Update vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const Prism = require('prismjs');
-const loadLanguages = require('prismjs/components/');
+const { defineUserConfig } = require('vuepress');
+const { docsearchPlugin } = require('@vuepress/plugin-docsearch');
+const { viteBundler } = require('@vuepress/bundler-vite');
+const sidebar = require('./sidebar');
+const theme = require('./theme');
+const fathomPlugin = require('./plugins/fathom');
+const markdownPlugin = require('./plugins/markdown');
 
-loadLanguages(['php']);
 
 const demoEnvPath = path.resolve(__dirname, '../../demo/.env');
 
@@ -25,17 +29,15 @@ const {
 
 const DOCS_HOME_URL = DOCS_MAIN_URL === APP_URL ? '/' : DOCS_MAIN_URL;
 
-module.exports = {
+module.exports = defineUserConfig({
     title: DOCS_TITLE,
     base: '/docs/',
     head: [
         ['link', { rel: 'icon', type:'image/png', href: '/docs/favicon.png' }],
         ['link', { rel: 'icon', type:'image/svg+xml', href: '/docs/favicon.svg' }],
     ],
-    theme: path.resolve(__dirname, './theme'),
-    themeConfig: {
+    theme: theme({
         logo: '/logo.svg',
-        logoDark: '/logo-dark.svg',
         navbar: [
             DOCS_ENABLE_VERSIONING === 'true' && {
                 text: DOCS_VERSION,
@@ -54,158 +56,30 @@ module.exports = {
                 ]
             }
         ].filter(Boolean),
-        sidebar: [
-            {
-                text: 'Introduction',
-                children: [
-                    'README.md',
-                    'entity-class.md',
-                ].map(page => `/guide/${page}`),
-            },
-            {
-                text: 'Entity Lists',
-                children: [
-                    'building-entity-list.md',
-                    'filters.md',
-                    'entity-states.md',
-                    'reordering-instances.md',
-                ].map(page => `/guide/${page}`),
-            },
-            {
-                text: 'Forms',
-                children: [
-                    'building-form.md',
-                    'multiforms.md',
-                    'single-form.md',
-                    'custom-form-fields.md',
-                    'form-editor-embeds.md'
-                ].map(page => `/guide/${page}`),
-            },
-            {
-                text: 'Show Pages',
-                children: [
-                    'building-show-page.md',
-                    'single-show.md',
-                    'custom-show-fields.md'
-                ].map(page => `/guide/${page}`),
-            },
-            {
-                text: 'Dashboards',
-                children: [
-                    '/guide/building-dashboard.md',
-                    ...[
-                        'graph.md',
-                        'panel.md',
-                        'ordered-list.md',
-                    ].map(page => `/guide/dashboard-widgets/${page}`),
-                ],
-            },
-            {
-                text: 'Commands',
-                children: [
-                    'commands.md',
-                    'commands-wizard.md',
-                ].map(page => `/guide/${page}`),
-            },
-            {
-                text: 'Authentication and authorizations',
-                children: [
-                    'authentication.md',
-                    'entity-authorizations.md',
-                ].map(page => `/guide/${page}`),
-            },
-            {
-                text: 'Generalities',
-                children: [
-                    'building-menu.md',
-                    'sharp-breadcrumb.md',
-                    'how-to-transform-data.md',
-                    'link-to.md',
-                    'page-alerts',
-                    'context.md',
-                    'sharp-uploads.md',
-                    'data-localization.md',
-                    'testing-with-sharp.md',
-                    'artisan-generators.md',
-                    'style-visual-theme.md'
-                ].map(page => `/guide/${page}`),
-            },
-            {
-                text: 'Form fields',
-                children: [
-                    'text.md',
-                    'textarea.md',
-                    'editor.md',
-                    'number.md',
-                    'html.md',
-                    'check.md',
-                    'date.md',
-                    'upload.md',
-                    'select.md',
-                    'autocomplete.md',
-                    'tags.md',
-                    'list.md',
-                    'autocomplete-list.md',
-                    'geolocation.md',
-                ].map(page => `/guide/form-fields/${page}`),
-            },
-            {
-                text: 'Show fields',
-                children: [
-                    'text.md',
-                    'picture.md',
-                    'list.md',
-                    'file.md',
-                    'embedded-entity-list.md',
-                ].map(page => `/guide/show-fields/${page}`),
-            },
-            {
-                text: 'Migrations guide',
-                children: [
-                    '7.0.md',
-                    '6.0.md',
-                    '5.0.md',
-                    '4.2.md',
-                    '4.1.3.md',
-                    '4.1.md',
-                ].map(page => `/guide/upgrading/${page}`),
-            },
-        ],
-    },
+
+        sidebar,
+    }),
     plugins: [
-        [
-            '@vuepress/plugin-docsearch',
-            {
-                appId: '1A1N8XRQFM',
-                apiKey: 'c5c8c8034f3c0586d562fdbb0a4d26cb',
-                indexName: 'code16_sharp',
-                searchParameters: {
-                    facetFilters: [`tags:${DOCS_ALGOLIA_TAG}`],
-                },
-            }
-        ],
-        {
-            extendsMarkdown: md => {
-                md.renderer.rules['code_inline'] = (tokens, idx, options, env, slf) => {
-                    const token = tokens[idx];
-                    const highlighted = Prism.highlight(token.content, Prism.languages.php);
-                        const inlineNodes = tokens.filter(token =>
-                    token.type === 'text' && token.content.trim()
-                    || token.type === 'code_inline'
-                );
-                const isFullwidth = inlineNodes.length === 1;
-                return `<code class="inline ${isFullwidth ? 'full' : ''}" v-pre>${highlighted}</code>`;
-                };
-            }
-        },
+        docsearchPlugin({
+            appId: '1A1N8XRQFM',
+            apiKey: 'c5c8c8034f3c0586d562fdbb0a4d26cb',
+            indexName: 'code16_sharp',
+            searchParameters: {
+                facetFilters: [`tags:${DOCS_ALGOLIA_TAG}`],
+            },
+        }),
+        fathomPlugin({
+            siteId: 'EELMENOG',
+            domains: '*.code16.fr',
+        }),
+        markdownPlugin,
     ],
-    bundler: '@vuepress/bundler-vite',
-    bundlerConfig: {
+    bundler: viteBundler({
         viteOptions: {
             plugins: [require('vite-svg-loader')({ svgo: false })],
         },
-    },
+    }),
     // scss: {
     //     implementation: require('sass'),
     // },
-};
+});
