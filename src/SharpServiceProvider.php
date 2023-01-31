bd1160f3aa2ee750035f397cfe874a5a96bbc962
philippe <philippe@code16.fr>
2023-01-31 11:44:56 +0100

    Version

--- src/SharpServiceProvider.php
+++ src/SharpServiceProvider.php
@@ -36,7 +36,7 @@ use Intervention\Image\ImageServiceProviderLaravelRecent;
 
 class SharpServiceProvider extends ServiceProvider
 {
-    const VERSION = '7.23.2';
+    const VERSION = '7.24.0';
 
     public function boot()
     {
