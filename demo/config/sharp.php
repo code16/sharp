3fd3e505065141449663e961f61e3ea91bd4393f
philippe <philippe@code16.fr>
2023-04-21 16:08:31 +0200

    Allow closure as global filter config

--- demo/config/sharp.php
+++ demo/config/sharp.php
@@ -16,9 +16,7 @@ return [
         'test' => \App\Sharp\Entities\TestEntity::class,
     ],
 
-    'global_filters' => [
-        'f' => \App\Sharp\MyGlobalFilter::class
-    ],
+    'global_filters' => fn() => auth()->id() === 1 ? [] : ['f' => \App\Sharp\MyGlobalFilter::class],
 
     'menu' => \App\Sharp\SharpMenu::class,
 
