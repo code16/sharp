4c5f798154a7bfdcc5b89cdc7a047279f1debdf9
philippe <philippe@code16.fr>
2023-05-15 14:46:17 +0200

    Allow closure in config `sharp.auth.login_page_url`

--- src/SharpServiceProvider.php
+++ src/SharpServiceProvider.php
@@ -36,7 +36,7 @@ use Intervention\Image\ImageServiceProviderLaravelRecent;
 
 class SharpServiceProvider extends ServiceProvider
 {
-    const VERSION = '7.28.0';
+    const VERSION = '7.28.1';
 
     public function boot()
     {
