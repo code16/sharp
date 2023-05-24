2a13668114727ebad43f95199aa08ee9f636ac8f
philippe <philippe@code16.fr>
2023-05-24 16:27:15 +0200

    Test DashboardLayoutSection

--- tests/Feature/Api/DashboardControllerTest.php
+++ tests/Feature/Api/DashboardControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Code16\Sharp\Tests\Feature\Api;
 
+use Code16\Sharp\Tests\Fixtures\SharpDashboardWithSections;
+
 class DashboardControllerTest extends BaseApiTest
 {
     protected function setUp(): void
@@ -18,14 +20,17 @@ class DashboardControllerTest extends BaseApiTest
         $this->buildTheWorld();
 
         $this->getJson(route('code16.sharp.api.dashboard', 'personal_dashboard'))
-            ->assertStatus(200)
-            ->assertJson(['widgets' => [
-                'bars' => [
-                    'type' => 'graph',
-                ], 'panel' => [
-                    'type' => 'panel',
-                ],
-            ]]);
+            ->assertOk()
+            ->assertJson([
+                'widgets' => [
+                    'bars' => [
+                        'type' => 'graph',
+                    ], 
+                    'panel' => [
+                        'type' => 'panel',
+                    ],
+                ]
+            ]);
     }
 
     /** @test */
@@ -34,53 +39,63 @@ class DashboardControllerTest extends BaseApiTest
         $this->buildTheWorld();
 
         $this->getJson(route('code16.sharp.api.dashboard', 'personal_dashboard'))
-            ->assertStatus(200)
-            ->assertJson(['layout' => [
-                'rows' => [
-                    [
-                        ['key' => 'bars', 'size' => 12],
-                    ], [
-                        ['key' => 'panel', 'size' => 4],
-                        ['key' => 'bars2', 'size' => 8],
-                    ],
-                ],
-            ]]);
+            ->assertOk()
+            ->assertJson([
+                'layout' => [
+                    'sections' => [
+                        [
+                            'key' => null,
+                            'label' => 'one',
+                            'rows' => [
+                                [
+                                    ['key' => 'bars', 'size' => 12],
+                                ], [
+                                    ['key' => 'panel', 'size' => 4],
+                                    ['key' => 'bars2', 'size' => 8],
+                                ],
+                            ],
+                        ],
+                    ]
+                ]
+            ]);
     }
-
+    
     /** @test */
     public function we_can_get_dashboard_data()
     {
         $this->buildTheWorld();
 
         $this->getJson(route('code16.sharp.api.dashboard', 'personal_dashboard'))
-            ->assertStatus(200)
-            ->assertJson(['data' => [
-                'bars1' => [
-                    'key' => 'bars1',
-                    'datasets' => [
-                        [
-                            'data' => [10, 20, 30],
-                            'label' => 'Bars 1',
+            ->assertOk()
+            ->assertJson([
+                'data' => [
+                    'bars1' => [
+                        'key' => 'bars1',
+                        'datasets' => [
+                            [
+                                'data' => [10, 20, 30],
+                                'label' => 'Bars 1',
+                            ],
+                        ],
+                        'labels' => ['a', 'b', 'c'],
+                    ],
+                    'bars2' => [
+                        'key' => 'bars2',
+                        'datasets' => [
+                            [
+                                'data' => [10, 20, 30],
+                                'label' => 'Bars 2',
+                            ],
+                        ],
+                        'labels' => ['a', 'b', 'c'],
+                    ],
+                    'panel' => [
+                        'key' => 'panel',
+                        'data' => [
+                            'name' => 'John Wayne',
                         ],
                     ],
-                    'labels' => ['a', 'b', 'c'],
-                ],
-                'bars2' => [
-                    'key' => 'bars2',
-                    'datasets' => [
-                        [
-                            'data' => [10, 20, 30],
-                            'label' => 'Bars 2',
-                        ],
-                    ],
-                    'labels' => ['a', 'b', 'c'],
-                ],
-                'panel' => [
-                    'key' => 'panel',
-                    'data' => [
-                        'name' => 'John Wayne',
-                    ],
-                ],
-            ]]);
+                ]
+            ]);
     }
 }
