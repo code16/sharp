22c78682e624871ef5bdf70cddeef37e98ed3d39
antoine <antoine.guingand@wanadoo.fr>
2023-06-01 19:33:22 +0200

    Refactor to MenuManager

---
+++ src/Utils/Menu/SharpMenuManager.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Code16\Sharp\Utils\Menu;
+
+
+use Code16\Sharp\View\Components\Menu\MenuSection;
+use Illuminate\Support\Collection;
+
+class SharpMenuManager
+{
+    protected ?SharpMenu $menu = null;
+
+    public function menu(): ?SharpMenu
+    {
+        if ($this->menu === null) {
+            $this->buildMenu();
+        }
+
+        return $this->menu;
+    }
+
+    public function userMenu(): ?SharpMenuUserMenu
+    {
+        return $this->menu()?->userMenu();
+    }
+
+    public function getItems(): Collection
+    {
+        return $this->menu()
+            ?->items()
+            ->filter(function (SharpMenuItem $item) {
+                if($item->isSection()) {
+                    return count($this->resolveSectionItems($item)) > 0;
+                }
+                return $item->isAllowed();
+            }) ?? collect();
+    }
+
+    public function getFlattenedItems(): Collection
+    {
+        return $this->getItems()
+            ->map(function (SharpMenuItem $item) {
+               return $item->isSection()
+                   ? $this->resolveSectionItems($item)
+                   : $item;
+            })
+            ->flatten();
+    }
+
+    public function getEntityMenuItem(string $entityKey): ?SharpMenuItemLink
+    {
+        return $this->getFlattenedItems()
+            ->first(function (SharpMenuItem $item) use ($entityKey) {
+                return $item->isEntity()
+                    && $item->getEntityKey() === $entityKey;
+            });
+    }
+
+    public function resolveSectionItems(SharpMenuItemSection $section): Collection
+    {
+        return collect($section->getItems())
+            ->filter(fn (SharpMenuItem $item) => $item->isAllowed())
+            ->pipe(fn ($items) => $this->filterSeparators($items));
+    }
+
+    protected function buildMenu(): void
+    {
+        if (($sharpMenu = config('sharp.menu')) === null) {
+            $this->menu = null;
+            return;
+        }
+
+        $this->menu = is_string($sharpMenu)
+            ? app($sharpMenu)
+            : $sharpMenu;
+
+        $this->menu->build();
+    }
+
+    private function filterSeparators(Collection $items): Collection
+    {
+        $filtered = collect();
+
+        foreach ($items->reverse() as $item) {
+            if ($item->isSeparator()) {
+                // Prevent separators in last position or stacked
+                if (count($filtered) === 0 || $filtered->last()?->isSeparator()) {
+                    continue;
+                }
+            }
+            $filtered->push($item);
+        }
+
+        return $filtered->reverse();
+    }
+}
--- src/View/Components/Menu.php
+++ src/View/Components/Menu.php
@@ -2,29 +2,33 @@
 
 namespace Code16\Sharp\View\Components;
 
+
 use Code16\Sharp\Utils\Menu\SharpMenuItem;
 use Code16\Sharp\Utils\Menu\SharpMenuItemLink;
+use Code16\Sharp\Utils\Menu\SharpMenuManager;
 use Code16\Sharp\View\Components\Menu\MenuSection;
 use Illuminate\Support\Collection;
 use Illuminate\View\Component;
 
 class Menu extends Component
 {
-    use HasSharpMenu;
-
     public string $title;
     public ?string $currentEntityKey;
     public ?SharpMenuItemLink $currentEntityItem;
     public bool $hasGlobalFilters;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->title = config('sharp.name', 'Sharp');
         $this->currentEntityKey = currentSharpRequest()->breadcrumb()->first()->key ?? null;
         $this->currentEntityItem = $this->currentEntityKey
-            ? $this->getEntityMenuItem($this->currentEntityKey)
+            ? app(SharpMenuManager::class)->getEntityMenuItem($this->currentEntityKey)
             : null;
         $this->hasGlobalFilters = sizeof(value(config('sharp.global_filters')) ?? []) > 0;
+    }
+
+    public function getItems(): Collection
+    {
+        return app(SharpMenuManager::class)->getItems();
     }
 
     public function render()
@@ -32,34 +36,5 @@ class Menu extends Component
         return view('sharp::components.menu', [
             'self' => $this,
         ]);
-    }
-
-    public function getItems(): Collection
-    {
-        return $this
-            ->getSharpMenu()
-            ?->items()
-            ->filter(fn (SharpMenuItem $item) => $item->isSection()
-                ? count((new MenuSection($item))->getItems()) > 0
-                : $item->isAllowed()
-            ) ?? collect();
-    }
-
-    public function getEntityMenuItem(string $entityKey): ?SharpMenuItemLink
-    {
-        return $this->getFlattenedItems()
-            ->first(fn (SharpMenuItem $item) => $item->isEntity()
-                && $item->getEntityKey() === $entityKey
-            );
-    }
-
-    public function getFlattenedItems(): Collection
-    {
-        return $this->getItems()
-            ->map(fn (SharpMenuItem $item) => $item->isSection()
-                ? (new MenuSection($item))->getItems()
-                : $item
-            )
-            ->flatten();
     }
 }
