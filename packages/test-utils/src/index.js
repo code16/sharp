2b9b9d7b529fe1acb04dc380d7046193e1445a67
antoine <antoine.guingand@wanadoo.fr>
2023-07-24 14:08:53 +0200

    Fix front tests

--- packages/test-utils/src/index.js
+++ packages/test-utils/src/index.js
@@ -1,8 +1,8 @@
-export { default as MockInjections } from './MockInjections.vue';
-export { default as MockI18n } from './MockI18n.vue';
-export { default as QueryComponent } from './QueryComponent.vue';
-export { default as MockTransitions } from './MockTransitions.vue';
-export { mockSFC, unmockSFC } from './mockSFC.vue';
+export { default as MockInjections } from './MockInjections';
+export { default as MockI18n } from './MockI18n';
+export { default as QueryComponent } from './QueryComponent';
+export { default as MockTransitions } from './MockTransitions';
+export { mockSFC, unmockSFC } from './mockSFC';
 export { mockProperty, unmockProperty, setter } from './mock-utils';
 export { nextRequestFulfilled } from './moxios-utils';
 export { createStub } from './stubs';
