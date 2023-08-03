49dbaa464e5f124a08607d0e8e37b24a7e57f6df
antoine <antoine.guingand@wanadoo.fr>
2023-08-03 17:38:46 +0200

    Tailwind demo

--- demo/resources/views/sharp/_login-page-message.blade.php
+++ demo/resources/views/sharp/_login-page-message.blade.php
@@ -1,33 +1,33 @@
 
-<div class="card small border-0 text-center">
-    <div class="card-body">
-        <div class="fw-bold mb-2">
-            Use these accounts to login&nbsp;:
-        </div>
-        <button
-            class="btn btn-text btn-sm"
-            type="button"
-            onclick="fillForm('admin@example.org')"
-        >
-            Admin
-        </button>
-        <button
-            class="btn btn-text btn-sm"
-            type="button"
-            onclick="fillForm('editor@example.org')"
-        >
-            Editor
-        </button>
+<div class="bg-white rounded p-4 text-center">
+    <div class="text-sm mb-2">
+        Use these accounts to login&nbsp;:
     </div>
+    <button
+        class="rounded-md bg-indigo-50 px-3 py-2 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
+        type="button"
+        onclick="fillForm('admin@example.org')"
+    >
+        Admin
+    </button>
+    <button
+        class="rounded-md bg-indigo-50 px-3 py-2 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
+        type="button"
+        onclick="fillForm('editor@example.org')"
+    >
+        Editor
+    </button>
 </div>
 
 @push('script')
     <script>
         function fillForm(email) {
-            document.querySelector('#login').setAttribute('value', email);
-            document.querySelector('#password').setAttribute('value', 'password');
+            document.querySelector('#login').value = email;
+            document.querySelector('#password').value = 'password';
         }
 
-        fillForm('admin@example.org');
+        window.addEventListener('sharp:mounted', () => {
+            fillForm('admin@example.org');
+        });
     </script>
 @endpush
---
+++ demo/tailwind.sharp.config.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+    content: [
+        './resources/views/sharp/**/*.blade.php',
+    ],
+    plugins: [
+    ],
+};
