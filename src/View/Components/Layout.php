1e02f2969d8881d017dd43731d07bf5cb25edf94
antoine <antoine.guingand@wanadoo.fr>
2023-08-03 17:38:34 +0200

    Move demo to vite

--- src/View/Components/Layout.php
+++ src/View/Components/Layout.php
@@ -10,10 +10,6 @@ class Layout extends Component
 {
     public function render(): View
     {
-        if (Vite::hotFile() === public_path('/hot')) {
-            Vite::useHotFile(public_path('vendor/sharp/hot')); // allow running "npm run dev" for symlinked assets
-        }
-
         return view('sharp::layouts.app');
     }
 }
