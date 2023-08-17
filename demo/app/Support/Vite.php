f82f564c511b3273bebdaec903359b213d5bb864
antoine <antoine.guingand@wanadoo.fr>
2023-08-17 16:36:04 +0200

    Fix vite

--- demo/app/Support/Vite.php
+++ demo/app/Support/Vite.php
@@ -7,7 +7,7 @@ class Vite extends \Illuminate\Foundation\Vite
     public function __invoke($entrypoints, $buildDirectory = null)
     {
         if ($buildDirectory === '/vendor/sharp') {
-            $this->useHotFile(base_path('../resources/assets/dist/hot')); // allow "npm run dev" (in sharp directory)
+            $this->useHotFile(base_path('../dist/hot')); // allow "npm run dev" (in sharp directory)
         }
 
         return tap(parent::__invoke($entrypoints, $buildDirectory), function () {
