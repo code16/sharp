729e65d3922091c551797b9bf751a2b1ea40202b
philippe <philippe@code16.fr>
2023-09-05 16:15:03 +0200

    SharpShow tests

---
+++ tests/Unit/Show/Utils/FakeSharpSingleShow.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Code16\Sharp\Tests\Unit\Show\Utils;
+
+use Code16\Sharp\Show\Layout\ShowLayout;
+use Code16\Sharp\Show\SharpShow;
+use Code16\Sharp\Show\SharpSingleShow;
+use Code16\Sharp\Utils\Fields\FieldsContainer;
+
+class FakeSharpSingleShow extends SharpSingleShow
+{
+    protected function buildShowFields(FieldsContainer $showFields): void
+    {
+    }
+
+    protected function buildShowLayout(ShowLayout $showLayout): void
+    {
+    }
+
+    public function findSingle(): array
+    {
+    }
+}
