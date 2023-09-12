d8588073c3bddfdfc89f6cc2f8c67ea8a630c962
antoine <antoine.guingand@wanadoo.fr>
2023-09-12 11:43:50 +0200

    Cleanup

--- resources/js/components/Notifications.vue
+++ resources/js/components/Notifications.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import { usePage } from "@inertiajs/vue3";
+    import { NotificationData } from "@/types";
 
     const notifications = usePage().props.notifications as NotificationData[]
 
