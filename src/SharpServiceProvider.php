75cf1c9fb38c86ed49cc1488b5210ad83b8dabde
antoine <antoine.guingand@wanadoo.fr>
2023-09-25 15:45:03 +0200

    Version

--- src/SharpServiceProvider.php
+++ src/SharpServiceProvider.php
@@ -38,7 +38,7 @@ use Intervention\Image\ImageServiceProviderLaravelRecent;
 
 class SharpServiceProvider extends ServiceProvider
 {
-    const VERSION = '8.0.5';
+    const VERSION = '8.0.6';
 
     public function boot()
     {
