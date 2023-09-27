1e8c25aefd2427059a1afb94779ee81058f82b9a
antoine <antoine.guingand@wanadoo.fr>
2023-09-27 21:37:43 +0200

    WIP

--- packages/form/src/components/ui/FormLayout.vue
+++ packages/form/src/components/ui/FormLayout.vue
@@ -1,52 +1,81 @@
 <script setup lang="ts">
-    import { onUnmounted, ref } from "vue";
-    import { Tab, Tabs } from "@sharp/ui";
-    import { FormLayoutData, FormLayoutTabData } from "@/types";
+    import { onUnmounted, ref, watchEffect } from "vue";
     import { slugify } from "@/utils";
+    import { Form } from "../../Form";
+    import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/vue";
 
-    defineProps<{ layout:FormLayoutData }>();
+    const props = defineProps<{
+        form: Form,
+    }>();
+    const selectedIndex = ref(0);
 
-    const query = ref(new URLSearchParams(location.search));
-    const onPopState = () => {
-        query.value = new URLSearchParams(location.search);
+    function onTabChange(index: number) {
+        selectedIndex.value = index;
+    }
+
+    function onPopState() {
+        selectedIndex.value = Math.max(0, props.form.layout.tabs
+            .findIndex(tab => new URLSearchParams(location.search).get('tab') == slugify(tab.title))
+        );
     }
     window.addEventListener('popstate', onPopState);
     onUnmounted(() => window.removeEventListener('popstate', onPopState));
-
-    function isActive(tab: FormLayoutTabData) {
-        return query.value.get('tab') === slugify(tab.title);
-    }
-
-    function onTabClicked() {
-        history.replaceState(null, null, `?${query.value}`);
-    }
+    watchEffect(() => {
+        if(props.form.layout.tabs.length > 1) {
+            history.replaceState(null, null, `?tab=${slugify(props.form.layout.tabs[selectedIndex.value].title)}`);
+        }
+    });
 </script>
 <template>
-    <div class="SharpTabbedLayout">
-        <template v-if="layout.tabbed && layout.tabs.length > 1">
-            <Tabs nav-class="bg-white border-bottom p-3 pb-0">
-                <template v-for="(tab, i) in layout.tabs" :key="`tab-${i}`">
-                    <Tab :title="tab.title" :active="isActive(tab)" @active="onTabClicked(tab)">
-                        <slot :tab="tab" />
-                    </Tab>
-                </template>
-                <template v-slot:nav-append>
-                    <slot name="nav-append" />
-                </template>
-            </Tabs>
-        </template>
-        <template v-else>
-            <template v-if="$slots['nav-append']">
-                <div class="d-flex justify-content-end bg-white border-bottom p-3">
-                    <slot name="nav-append" />
-                </div>
-            </template>
-            <div class="tab-pane">
-                <template v-for="tab in layout.tabs">
-                    <slot :tab="tab" />
-                    <hr class="SharpTabbedLayout__divider">
-                </template>
+    <template v-if="form.layout.tabbed && form.layout.tabs.length > 1">
+        <TabGroup :selected-index="selectedIndex" @change="onTabChange">
+            <div class="sm:hidden">
+                <select
+                    class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-i-500 focus:outline-none focus:ring-i-500 sm:text-sm"
+                    :value="selectedIndex"
+                    @change="onTabChange($event.target.value)"
+                >
+                    <template v-for="(tab, i) in form.layout.tabs">
+                        <option :value="i">{{ tab.title }}</option>
+                    </template>
+                </select>
             </div>
+            <div class="hidden sm:block">
+                <div class="border-b border-gray-200">
+                    <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+                        <TabList>
+                            <template v-for="tab in form.layout.tabs">
+                                <Tab v-slot="{ selected }">
+                                    <button :class="[selected ? 'border-primary-500 text-primary-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700', 'flex whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium']">
+                                        {{ tab.title }}
+
+                                        <template v-if="form.tabHasError(tab)">
+                                            <svg class="h-1.5 w-1.5 fill-red-500" viewBox="0 0 6 6" aria-hidden="true">
+                                                <circle cx="3" cy="3" r="3" />
+                                            </svg>
+                                        </template>
+                                    </button>
+                                </Tab>
+                            </template>
+                        </TabList>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="bg-white p-4">
+                <TabPanels>
+                    <template v-for="tab in form.layout.tabs">
+                        <TabPanel>
+                            <slot :tab="tab" />
+                        </TabPanel>
+                    </template>
+                </TabPanels>
+            </div>
+        </TabGroup>
+    </template>
+    <template v-else>
+        <template v-for="tab in form.layout.tabs">
+            <slot :tab="tab" />
         </template>
-    </div>
+    </template>
 </template>
