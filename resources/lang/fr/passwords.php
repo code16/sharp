99b03a3a981c9c04838ed99a6b9bccea66cc4ae5
antoine <antoine.guingand@wanadoo.fr>
2023-10-24 12:28:55 +0200

    Cleanup langs

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé par email le lien de réinitialisation de votre mot de passe ! (Si un utilisateur est associé à cette adresse email)',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => 'Impossible de trouver un compte avec cette adresse email.',
+];
