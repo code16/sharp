837517110bc915650d6e6faafe6688f9c57c8a42
antoine <antoine.guingand@wanadoo.fr>
2023-12-04 11:26:15 +0100

    Fix search clearing on button click (safari)

--- packages/ui/src/components/Search.vue
+++ packages/ui/src/components/Search.vue
@@ -32,7 +32,7 @@
                     </button>
                 </div>
 
-                <button class="btn btn-sm btn-outline-primary d-inline-flex SharpSearch__button">
+                <button class="btn btn-sm btn-outline-primary d-inline-flex SharpSearch__button" tabindex="0">
                     <!-- heroicons: solid/20/search -->
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
