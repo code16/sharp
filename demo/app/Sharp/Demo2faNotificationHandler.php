703656d1ada47b8ac237c8012b13528274fb1f9f
Arnaud Becher <arnaud.becher@gmail.com>
2023-12-12 11:48:52 +0100

    enabled 2fa for all users on demo + better infobox and help text to help understand what's happening

--- demo/app/Sharp/Demo2faNotificationHandler.php
+++ demo/app/Sharp/Demo2faNotificationHandler.php
@@ -6,11 +6,6 @@ use Code16\Sharp\Auth\TwoFactor\Sharp2faNotificationHandler;
 
 class Demo2faNotificationHandler extends Sharp2faNotificationHandler
 {
-    public function isEnabledFor($user): bool
-    {
-        return $user->email === 'editor@example.org';
-    }
-
     protected function generateRandomCode(): int
     {
         return 123456;
@@ -19,8 +14,12 @@ class Demo2faNotificationHandler extends Sharp2faNotificationHandler
     public function formHelpText(): string
     {
         return <<<'HTML'
-                <div>This user has configured a two-factor authentication.</div>
-                <div class="mt-2">Code was set to <strong>123456</strong></div>
+                <div class="alert alert-info">
+                    <div>Two-factor authentication based on email notifications has been enabled for all users.</div>
+                    <div class="mt-2">Expected code is <strong>123456</strong>.</div>
+                    <div class="mt-2">It is also possible to set an authentification based on a TOTP authenticator app (like Google or Microsoft Authenticator). Check the <a href="https://sharp.code16.fr/docs/guide/authentication.html#two-factor-authentication" target="_blank">documentation</a>.</div>
+                </div>
+                An email has been sent to your address. Please enter the 6-digit code.
             HTML;
     }
 }
