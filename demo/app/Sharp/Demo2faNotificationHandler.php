52f4615060b088160301c348210f444bb28aaf3c
Arnaud Becher <arnaud.becher@gmail.com>
2023-12-12 18:31:29 +0100

    even shorter

--- demo/app/Sharp/Demo2faNotificationHandler.php
+++ demo/app/Sharp/Demo2faNotificationHandler.php
@@ -15,7 +15,7 @@ class Demo2faNotificationHandler extends Sharp2faNotificationHandler
     {
         return <<<'HTML'
                 <div class="alert alert-info">
-                    <div><a href="https://sharp.code16.fr/docs/guide/authentication.html#two-factor-authentication" target="_blank">Two-factor authentication</a> comes with two flavours: notification (mail, sms, etc) or TOTP authenticator app (like Google Authenticator).</div>
+                    <div><a href="https://sharp.code16.fr/docs/guide/authentication.html#two-factor-authentication" target="_blank">Two-factor authentication</a> comes with two flavours: notification (mail, sms, etc) or TOTP app (like Google Authenticator).</div>
                     <div class="mt-2">This is a demo, expected code is <strong>123456</strong>.</div>
                 </div>
                 Please enter the 6-digit code
