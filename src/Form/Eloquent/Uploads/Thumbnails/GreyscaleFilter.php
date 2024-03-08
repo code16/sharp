c8ab5c2da852e3aa6fa1e3f2e9f05cab932c1444
philippe <philippe@code16.fr>
2024-03-08 12:57:28 +0100

    Upgrade to image/intervention 3

--- src/Form/Eloquent/Uploads/Thumbnails/GreyscaleFilter.php
+++ src/Form/Eloquent/Uploads/Thumbnails/GreyscaleFilter.php
@@ -4,18 +4,14 @@ namespace Code16\Sharp\Form\Eloquent\Uploads\Thumbnails;
 
 use Intervention\Image\Image;
 
+/**
+ * @deprecated use GreyscaleModifier instead
+ */
 class GreyscaleFilter extends ThumbnailFilter
 {
-    /**
-     * Applies filter effects to given image.
-     *
-     * @param  Image  $image
-     * @return Image
-     */
     public function applyFilter(Image $image)
     {
-        $image->greyscale();
-
-        return $image;
+       return (new GreyscaleModifier($this->params))
+            ->apply($image);
     }
 }
