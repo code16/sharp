d7bfda09af2165c2af8d689f294cbf2e5a525ec1
antoine <antoine.guingand@wanadoo.fr>
2024-04-05 10:30:09 +0200

    wip

--- resources/js/Layouts/Auth/AuthCard.vue
+++ resources/js/Layouts/Auth/AuthCard.vue
@@ -2,7 +2,6 @@
 
 import { config } from "@/utils/config";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import SharpIcon from "@/svg/SharpIcon.vue";
 
 </script>
 
--- resources/js/components/ui/sonner/Sonner.vue
+++ resources/js/components/ui/sonner/Sonner.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { Toaster as Sonner, type ToasterProps } from 'vue-sonner'
+import { Check, CircleAlert, TriangleAlert } from "lucide-vue-next";
 
 const props = defineProps<ToasterProps>()
 </script>
@@ -10,7 +11,8 @@ const props = defineProps<ToasterProps>()
     v-bind="props"
     :toast-options="{
       classes: {
-        toast: 'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+        toast: 'group toast data-[type=error]:border-destructive group-[.toaster]:items-start group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+        title: 'group-[[data-type=error]]:text-destructive',
         description: 'group-[.toast]:text-muted-foreground',
         actionButton:
           'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
@@ -18,5 +20,15 @@ const props = defineProps<ToasterProps>()
           'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
       },
     }"
-  />
+  >
+      <template #success-icon>
+          <Check class="w-4 h-4 mt-1" />
+      </template>
+      <template #error-icon>
+          <CircleAlert class="w-4 h-4 mt-1 text-destructive" />
+      </template>
+      <template #warning-icon>
+          <TriangleAlert class="w-4 h-4 mt-1" />
+      </template>
+  </Sonner>
 </template>
