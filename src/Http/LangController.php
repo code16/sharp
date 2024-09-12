5e42f7f6d201eba4f6df6e4d742145a7baaf7827
PatrickePatate <lucienpuget1@gmail.com>
2024-09-12 12:08:25 +0200

    Fix locale in asset controller

--- src/Http/LangController.php
+++ src/Http/LangController.php
@@ -2,6 +2,7 @@
 
 namespace Code16\Sharp\Http;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Cache;
 
@@ -11,9 +12,9 @@ class LangController extends Controller
      * Echoes out the localization messages as a JS file,
      * to be used by the front code (Vue.js).
      */
-    public function index()
+    public function index(Request $request)
     {
-        $lang = app()->getLocale();
+        $lang = $request->has('locale') ? $request->input('locale') : app()->getLocale();
         $version = sharp_version();
 
         $strings = Cache::rememberForever("sharp.lang.$lang.$version.js", function () {
