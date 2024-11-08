504b7999d55bdd7ff42172815695376ddb9b3222
philippe <philippe@code16.fr>
2024-11-08 13:50:51 +0100

    Version

--- src/SharpServiceProvider.php
+++ src/SharpServiceProvider.php
@@ -38,7 +38,7 @@ use Intervention\Image\ImageManager;
 
 class SharpServiceProvider extends ServiceProvider
 {
-    const VERSION = '8.5.0';
+    const VERSION = '8.6.0';
 
     public function boot()
     {
