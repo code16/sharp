d293df4a1c3651103e9dc0ffb3f06e068dda9470
antoine <guingand.antoine@gmail.com>
2024-11-21 16:09:15 +0100

    fix collection conversion to string

--- src/Utils/Fields/Formatters/FormatsLocalizedValue.php
+++ src/Utils/Fields/Formatters/FormatsLocalizedValue.php
@@ -6,6 +6,7 @@ use Closure;
 use Code16\Sharp\Form\Fields\SharpFormField;
 use Code16\Sharp\Show\Fields\SharpShowField;
 use Code16\Sharp\Utils\Fields\IsSharpFieldWithLocalization;
+use Illuminate\Contracts\Support\Arrayable;
 
 trait FormatsLocalizedValue
 {
@@ -21,11 +22,12 @@ trait FormatsLocalizedValue
     /**
      * @param  ?Closure<string>  $transformContent
      */
-    protected function maybeLocalized(SharpFormField|SharpShowField $field, array|string|null $value, ?Closure $transformContent = null): array|string|null
+    protected function maybeLocalized(SharpFormField|SharpShowField $field, array|Arrayable|string|null $value, ?Closure $transformContent = null): array|string|null
     {
         $transformContent ??= fn ($value) => $value;
 
         if ($field instanceof IsSharpFieldWithLocalization && $field->isLocalized()) {
+            $value = $value instanceof Arrayable ? $value->toArray() : $value;
             return collect([
                 ...collect($this->dataLocalizations ?? [])->mapWithKeys(fn ($locale) => [$locale => null]),
                 ...is_array($value) ? $value : [app()->getLocale() => $value],
