d5bcde6710aa52badb4f90c5ab4ae5829ecd7379
antoine <guingand.antoine@gmail.com>
2024-12-04 15:12:28 +0100

    Fix SharpException HTTP status + only show renderer page in 500 + debug

---
+++ tests/Http/ExceptionTest.php
@@ -0,0 +1,102 @@
+<?php
+
+
+use Code16\Sharp\Exceptions\Auth\SharpAuthorizationException;
+use Code16\Sharp\Exceptions\Form\SharpApplicativeException;
+use Code16\Sharp\Tests\Fixtures\Entities\PersonEntity;
+use Code16\Sharp\Tests\Fixtures\Sharp\PersonList;
+use Inertia\Testing\AssertableInertia as Assert;
+
+beforeEach(function () {
+    sharp()->config()->addEntity('person', PersonEntity::class);
+    login();
+});
+
+it('renders inertia Exception', function () {
+    fakeListFor('person', new class() extends PersonList
+    {
+        public function getListData(): array
+        {
+            throw new Exception('Test error');
+        }
+    });
+    
+    $this->get('/sharp/s-list/person')
+        ->assertStatus(500)
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Error')
+                ->where('status', 500)
+                ->where('message', 'Test error')
+        );
+});
+
+it('renders inertia HttpException', function () {
+    fakeListFor('person', new class() extends PersonList
+    {
+        public function getListData(): array
+        {
+            abort(404, 'Not found Test error');
+        }
+    });
+    
+    $this->get('/sharp/s-list/person')
+        ->assertStatus(404)
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Error')
+            ->where('status', 404)
+            ->where('message', 'Not found Test error')
+        );
+});
+
+it('renders inertia SharpException', function () {
+    fakeListFor('person', new class() extends PersonList
+    {
+        public function getListData(): array
+        {
+            throw new SharpAuthorizationException('Unauthorized Test error');
+        }
+    });
+    
+    $this->get('/sharp/s-list/person')
+        ->assertStatus(403)
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Error')
+            ->where('status', 403)
+            ->where('message', 'Unauthorized Test error')
+        );
+    
+    fakeListFor('person', new class() extends PersonList
+    {
+        public function getListData(): array
+        {
+            throw new SharpApplicativeException('Applicative Test error');
+        }
+    });
+    
+    $this->get('/sharp/s-list/person')
+        ->assertStatus(417)
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Error')
+            ->where('status', 417)
+            ->where('message', 'Applicative Test error')
+        );
+});
+
+it('renders debug 500 error', function () {
+    fakeListFor('person', new class() extends PersonList
+    {
+        public function getListData(): array
+        {
+            new UnknownClass();
+            return [];
+        }
+    });
+    
+    config()->set('app.debug', true);
+    
+    $response = $this->get('/sharp/s-list/person')
+        ->assertStatus(500);
+    
+    // response is not rendered in inertia
+    expect(fn () => $response->assertInertia())->toThrow('Not a valid Inertia response.');
+});
