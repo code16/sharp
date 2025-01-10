9c8e2e0498414946d2d7e686fcf48219e3a07528
antoine <guingand.antoine@gmail.com>
2025-01-10 10:06:36 +0100

    Assets

--- dist/assets/CardDescription.vue_vue_type_script_setup_true_lang-CLVD6m1Q.js
+++
@@ -1,1 +0,0 @@
-import{d as o,o as t,A as r,q as n,V as c,u as l,z as p}from"./sharp-Bqh6l4AV.js";const u=o({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(a,m)=>(t(),r("div",{class:c(l(p)("text-sm text-muted-foreground",e.class))},[n(a.$slots,"default")],2))}});export{u as _};
