ed4debf71686e2214fcc661af57b842e4768543c
antoine <guingand.antoine@gmail.com>
2025-01-14 11:57:30 +0100

    Version

--- src/SharpInternalServiceProvider.php
+++ src/SharpInternalServiceProvider.php
@@ -50,7 +50,7 @@ use Intervention\Image\ImageManager;
 
 class SharpInternalServiceProvider extends ServiceProvider
 {
-    const VERSION = '9.0.0-beta.6';
+    const VERSION = '9.0.0-beta.7';
 
     public function boot()
     {
