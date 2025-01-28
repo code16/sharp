176d584683f530da1851c88613efa31f9f3996a1
antoine <guingand.antoine@gmail.com>
2025-01-28 10:52:42 +0100

    handle widget with link w/o title

--- resources/js/dashboard/components/widgets/Panel.vue
+++ resources/js/dashboard/components/widgets/Panel.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-    import { OrderedListWidgetData, PanelWidgetData } from "@/types";
+    import { PanelWidgetData } from "@/types";
     import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
     import MaybeInertiaLink from "@/components/MaybeInertiaLink.vue";
     import Content from "@/components/Content.vue";
     import { DashboardWidgetProps } from "@/dashboard/types";
+    import { __ }from '@/utils/i18n'
 
     const props = defineProps<DashboardWidgetProps<PanelWidgetData>>();
 </script>
 
 <template>
-    <Card class="relative">
+    <Card class="relative" :class="widget.link ? 'transition-colors hover:bg-muted/50' : ''">
         <template v-if="widget.title">
             <CardHeader>
                 <CardTitle class="text-base/none font-semibold tracking-tight">
@@ -23,6 +24,15 @@
                         {{ widget.title }}
                     </template>
                 </CardTitle>
+            </CardHeader>
+        </template>
+        <template v-else-if="widget.link">
+            <CardHeader class="pb-0">
+                <MaybeInertiaLink class="hover:underline" :href="widget.link">
+                    <span class="absolute inset-0"></span>
+                    <span class="sr-only">{{ __('sharp::dashboard.widget.link_label') }}</span>
+                    {{ widget.title }}
+                </MaybeInertiaLink>
             </CardHeader>
         </template>
         <CardContent>
