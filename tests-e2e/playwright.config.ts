d2c79fb8a6e761d39ba4e2e4b7a245179b7cdcdc
antoine <guingand.antoine@gmail.com>
2025-02-03 17:55:51 +0100

    update reporter

--- tests-e2e/playwright.config.ts
+++ tests-e2e/playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    process.env.CI ? ['github'] : ['list'],
+    ['html'],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
