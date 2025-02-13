fbdba9ec2ad65a4382ef315352807bdf0ff1ed93
aguingand <17453506+aguingand@users.noreply.github.com>
2025-02-13 10:32:16 +0000

    CS fix

--- src/Form/Eloquent/Uploads/Thumbnails/SharpImageManager.php
+++ src/Form/Eloquent/Uploads/Thumbnails/SharpImageManager.php
@@ -2,7 +2,6 @@
 
 namespace Code16\Sharp\Form\Eloquent\Uploads\Thumbnails;
 
-
 use Intervention\Image\ImageManager;
 use Intervention\Image\Interfaces\DecoderInterface;
 use Intervention\Image\Interfaces\DriverInterface;
@@ -12,27 +11,27 @@ use Intervention\Image\Interfaces\ImageManagerInterface;
 class SharpImageManager implements ImageManagerInterface
 {
     protected ImageManager $manager;
-    
+
     public function __construct()
     {
         $this->manager = new ImageManager(sharp()->config()->get('uploads.image_driver'));
     }
-    
+
     public function create(int $width, int $height): ImageInterface
     {
         return $this->manager->create($width, $height);
     }
-    
+
     public function read(mixed $input, array|string|DecoderInterface $decoders = []): ImageInterface
     {
         return $this->manager->read($input, $decoders);
     }
-    
+
     public function animate(callable $init): ImageInterface
     {
         return $this->manager->animate($init);
     }
-    
+
     public function driver(): DriverInterface
     {
         return $this->manager->driver();
