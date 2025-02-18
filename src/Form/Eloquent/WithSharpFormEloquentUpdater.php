e07006395ade815dfa97bb3a7070d484b7caf353
antoine <guingand.antoine@gmail.com>
2025-02-18 11:00:41 +0100

    Add $this->save types

--- src/Form/Eloquent/WithSharpFormEloquentUpdater.php
+++ src/Form/Eloquent/WithSharpFormEloquentUpdater.php
@@ -21,7 +21,9 @@ trait WithSharpFormEloquentUpdater
     }
 
     /**
-     * Update an Eloquent Model with $data (which is already Form Field formatted).
+     * @template TModel of Model
+     * @param TModel $instance
+     * @return TModel
      */
     public function save(Model $instance, array $data): Model
     {
