441e4433daed9ae49a58c1bdea8aaa307cfe15d0
antoine <guingand.antoine@gmail.com>
2025-02-21 11:50:00 +0100

    fix mondayFirst config

--- resources/js/form/components/fields/Date.vue
+++ resources/js/form/components/fields/Date.vue
@@ -218,7 +218,7 @@ function onMinuteChange(minute: string) {
                     :model-value="calendarDateValue"
                     v-model:placeholder="calendarViewingDate"
                     :locale="dateLocale"
-                    :week-starts-on="props.field.mondayFirst ? 1 : 0"
+                    :week-starts-on="props.field.mondayFirst ? 0 : 1"
                     @update:model-value="onCalendarDateChange"
                     class="p-3"
                 >
