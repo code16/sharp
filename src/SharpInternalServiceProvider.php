368fc9cc264a1c2c5695b54294982878e9fe7c3d
dvlpp <973325+dvlpp@users.noreply.github.com>
2025-02-25 09:51:34 +0000

    Update version to v9.1.1

--- src/SharpInternalServiceProvider.php
+++ src/SharpInternalServiceProvider.php
@@ -50,7 +50,7 @@ use Inertia\ServiceProvider as InertiaServiceProvider;
 
 class SharpInternalServiceProvider extends ServiceProvider
 {
-    const VERSION = '9.1.0';
+    const VERSION = '9.1.1';
 
     public function boot()
     {
