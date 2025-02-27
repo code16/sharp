a198f78c7a6ea77fb84db90504281067adde0e58
PatrickePatate <16721134+PatrickePatate@users.noreply.github.com>
2025-02-27 16:09:24 +0000

    CS fix

--- tests/Http/BreadcrumbTest.php
+++ tests/Http/BreadcrumbTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Code16\Sharp\Tests\Fixtures\Entities\PersonEntity;
-use Code16\Sharp\Tests\Fixtures\Entities\PersonWithDynamicLabelEntity;
 use Code16\Sharp\Tests\Fixtures\Entities\SinglePersonEntity;
 use Code16\Sharp\Tests\Fixtures\Sharp\PersonShow;
 use Code16\Sharp\Utils\Entities\SharpEntityManager;
