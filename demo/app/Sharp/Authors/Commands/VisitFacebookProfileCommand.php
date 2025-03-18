5d40d02e2f8e921eb426ce2d830d5c309f2fa5b5
philippe <philippe@code16.fr>
2025-03-18 16:19:19 +0100

    Handle reload param + demo use case

--- demo/app/Sharp/Authors/Commands/VisitFacebookProfileCommand.php
+++ demo/app/Sharp/Authors/Commands/VisitFacebookProfileCommand.php
@@ -2,22 +2,30 @@
 
 namespace App\Sharp\Authors\Commands;
 
+use App\Models\User;
 use Code16\Sharp\EntityList\Commands\InstanceCommand;
 
 class VisitFacebookProfileCommand extends InstanceCommand
 {
     public function label(): string
     {
-        return "Visit author's facebook profile";
+        return 'Visit author’s facebook profile';
     }
 
     public function buildCommandConfig(): void
     {
-        $this->configureDescription('You will leave sharp');
+        $this->configureDescription('You will leave Sharp');
     }
 
     public function execute(mixed $instanceId, array $data = []): array
     {
         return $this->link('https://facebook.com');
+    }
+
+    public function authorizeFor(mixed $instanceId): bool
+    {
+        return sharp()->context()
+            ->findListInstance($instanceId, fn ($id) => User::find($id))
+            ->hasVerifiedEmail();
     }
 }
