d7209589443c4228bdfe77bcd60cf6a5f3b9e252
aguingand <17453506+aguingand@users.noreply.github.com>
2025-04-29 13:43:16 +0000

    Update version to v9.3.4

--- src/SharpInternalServiceProvider.php
+++ src/SharpInternalServiceProvider.php
@@ -52,7 +52,7 @@ use Inertia\ServiceProvider as InertiaServiceProvider;
 
 class SharpInternalServiceProvider extends ServiceProvider
 {
-    const VERSION = '9.3.3';
+    const VERSION = '9.3.4';
 
     public function boot()
     {
