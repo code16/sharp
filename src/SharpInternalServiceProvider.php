672b7f90a93219b8e9c4d1453a3a3f1c635a9ebd
aguingand <17453506+aguingand@users.noreply.github.com>
2025-06-17 13:17:03 +0000

    Update version to v9.6.1

--- src/SharpInternalServiceProvider.php
+++ src/SharpInternalServiceProvider.php
@@ -56,7 +56,7 @@ use Laravel\Octane\Events\TickReceived;
 
 class SharpInternalServiceProvider extends ServiceProvider
 {
-    const VERSION = '9.6.0';
+    const VERSION = '9.6.1';
 
     public function boot()
     {
