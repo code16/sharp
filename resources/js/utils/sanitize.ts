c1fe5a8ace5ed8d6f30c9fb96ef755ea48a0d1b0
antoine <guingand.antoine@gmail.com>
2025-10-02 14:54:24 +0200

    Allow iframe in sanitized HTML

--- resources/js/utils/sanitize.ts
+++ resources/js/utils/sanitize.ts
@@ -3,10 +3,11 @@ import DOMPurify from 'dompurify';
 export function sanitize(html: string | null) {
     return html
         ? DOMPurify.sanitize(html, {
+            ADD_TAGS: ['iframe'],
             CUSTOM_ELEMENT_HANDLING: {
                 tagNameCheck: () => true,
                 attributeNameCheck: () => true,
-            }
+            },
         })
         : html;
 }
