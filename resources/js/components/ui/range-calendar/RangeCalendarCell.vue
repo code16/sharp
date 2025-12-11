a1ea6a1ecbb10e6b81481f3779cbc6d961e5eb1f
antoine <guingand.antoine@gmail.com>
2025-12-11 13:54:10 +0100

    simplify

--- resources/js/components/ui/range-calendar/RangeCalendarCell.vue
+++ resources/js/components/ui/range-calendar/RangeCalendarCell.vue
@@ -7,7 +7,7 @@ import {
     useForwardProps
 } from 'reka-ui'
 import { cn } from '@/utils/cn'
-import { isSameMonth, DateValue, compare } from '@internationalized/date';
+import { isSameMonth, DateValue } from '@internationalized/date';
 
 const props = defineProps<RangeCalendarCellProps & { class?: HTMLAttributes['class'], month: DateValue, monthIndex: number }>()
 
@@ -19,33 +19,23 @@ const delegatedProps = computed(() => {
 
 const forwardedProps = useForwardProps(delegatedProps)
 const rootContext = injectRangeCalendarRootContext();
-const currentMonthSelected = computed(() => rootContext.startValue.value?.set({ day: 0 }).compare(props.month.set({ day: 0 })) <= 0
-&& rootContext.endValue.value?.set({ day: 0 }).compare(props.month.set({ day: 0 })) >= 0 ? true : null);
+const isNextMonthOutsideSelected = computed(() =>  isSameMonth(props.date, props.month.add({ months: 1 }))
+    && rootContext.startValue.value?.set({ day: 0 }).compare(props.month.set({ day: 0 })) <= 0
+    && rootContext.endValue.value?.set({ day: 0 }).compare(props.month.set({ day: 0 })) > 0);
+const isPreviousMonthOutsideSelected = computed(() => isSameMonth(props.date, props.month.subtract({ months: 1 }))
+    && rootContext.startValue.value?.set({ day: 0 }).compare(props.month.set({ day: 0 })) < 0
+    && rootContext.endValue.value?.set({ day: 0 }).compare(props.month.set({ day: 0 })) >= 0);
 </script>
 
 <template>
   <RangeCalendarCell
     :class="cn('relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([data-selected][data-selection-end]:not([data-outside-view]))]:rounded-r-md [&:has([data-selected][data-selection-start]:not([data-outside-view]))]:rounded-l-md',
-    'data-selected:bg-accent',
-    '[&[data-selected]:not([data-current-month-selected])]:bg-transparent',
-    '[[data-outside-view][data-selected]~&[data-before-next-month]]:bg-accent',
-    '[[data-outside-view][data-selected]~&[data-before-next-month]:last-child,&[data-before-next-month][data-selected]:last-child]:bg-transparent',
-    '[[data-outside-view][data-selected]~&[data-before-next-month]:last-child,&[data-before-next-month][data-selected]:last-child]:bg-linear-to-r',
-    '[[data-outside-view][data-selected]~&[data-before-next-month]:last-child,&[data-before-next-month][data-selected]:last-child]:from-accent',
-    '[[data-outside-view][data-selected]~&[data-before-next-month]:last-child,&[data-before-next-month][data-selected]:last-child]:to-transparent',
-    '[&[data-after-previous-month]:has(~_[data-outside-view][data-selected])]:bg-accent',
-    '[&[data-after-previous-month]:first-child:has(~_[data-outside-view][data-selected]),&[data-after-previous-month][data-selected]:first-child]:bg-transparent',
-    '[&[data-after-previous-month]:first-child:has(~_[data-outside-view][data-selected]),&[data-after-previous-month][data-selected]:first-child]:bg-linear-to-l',
-    '[&[data-after-previous-month]:first-child:has(~_[data-outside-view][data-selected]),&[data-after-previous-month][data-selected]:first-child]:from-accent',
-    '[&[data-after-previous-month]:first-child:has(~_[data-outside-view][data-selected]),&[data-after-previous-month][data-selected]:first-child]:to-transparent',
+        rootContext.isSelected(props.date) ? 'bg-accent' : '',
+        isNextMonthOutsideSelected ? 'bg-accent last:bg-transparent last:bg-linear-to-r last:from-accent last:to-transparent' : '',
+        isPreviousMonthOutsideSelected ? 'bg-accent first:bg-transparent first:bg-linear-to-l first:from-accent first:to-transparent' : '',
         props.class
     )"
-      :data-selected="rootContext.isSelected(props.date) ? true : null"
-      :data-outside-view="!isSameMonth(props.date, props.month) ? true : null"
-      :data-current-month-selected="currentMonthSelected"
-      :data-before-next-month="currentMonthSelected && isSameMonth(props.date, props.month.add({ months: 1 })) ? true : null"
-      :data-after-previous-month="currentMonthSelected && isSameMonth(props.date, props.month.subtract({ months: 1 })) ? true : null"
-    v-bind="forwardedProps"
+      v-bind="forwardedProps"
   >
     <slot />
   </RangeCalendarCell>
