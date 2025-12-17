5a564e5653b13fb4aea2bcd164d619d61ab936f2
dvlpp <973325+dvlpp@users.noreply.github.com>
2025-12-17 12:44:53 +0000

    Assets

--- dist/assets/CardDescription.vue_vue_type_script_setup_true_lang-Bgkupnl3.js
+++
@@ -1,1 +0,0 @@
-import{d as o,o as t,A as r,q as n,V as c,e as l,z as p}from"./sharp-BQ_wZQE4.js";const i=o({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(a,m)=>(t(),r("div",{class:c(l(p)("text-sm text-muted-foreground",e.class))},[n(a.$slots,"default")],2))}});export{i as _};
