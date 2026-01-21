3a9794b12e12e77069105ff0f67dd18cdc2cb963
antoine <guingand.antoine@gmail.com>
2026-01-21 16:02:20 +0100

    Add authorizeForStep() method for wizard commands

--- src/Http/Controllers/Api/Commands/HandlesInstanceCommand.php
+++ src/Http/Controllers/Api/Commands/HandlesInstanceCommand.php
@@ -3,6 +3,7 @@
 namespace Code16\Sharp\Http\Controllers\Api\Commands;
 
 use Code16\Sharp\EntityList\Commands\InstanceCommand;
+use Code16\Sharp\EntityList\Commands\Wizards\InstanceWizardCommand;
 use Code16\Sharp\EntityList\SharpEntityList;
 use Code16\Sharp\Exceptions\Auth\SharpAuthorizationException;
 use Code16\Sharp\Show\SharpShow;
@@ -17,7 +18,11 @@ trait HandlesInstanceCommand
         $commandHandler = $commandContainer->findInstanceCommandHandler($commandKey);
         $commandHandler->buildCommandConfig();
 
-        if (! $commandHandler->authorize() || ! $commandHandler->authorizeFor($instanceId)) {
+        $authorized = $commandHandler instanceof InstanceWizardCommand && ($step = $commandHandler->extractStepFromRequest())
+            ? $commandHandler->authorizeForStep($step, $instanceId)
+            : $commandHandler->authorize() && $commandHandler->authorizeFor($instanceId);
+
+        if (! $authorized) {
             throw new SharpAuthorizationException();
         }
 
