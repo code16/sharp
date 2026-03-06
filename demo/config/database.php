f3a73fb1eeb4c61a63cd033bdd9940f0b8e3239e
PatrickePatate <lucienpuget1@gmail.com>
2026-03-06 14:23:53 +0100

    PHP8.5 in Demo

--- demo/config/database.php
+++ demo/config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
