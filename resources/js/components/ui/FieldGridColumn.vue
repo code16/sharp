1709cc3b305e1c57e6eecb6c827339195d54b424
antoine <guingand.antoine@gmail.com>
2026-04-23 12:07:31 +0200

    update to vite 8

--- resources/js/components/ui/FieldGridColumn.vue
+++ resources/js/components/ui/FieldGridColumn.vue
@@ -16,8 +16,3 @@
         <slot />
     </div>
 </template>
-<!--    <div class="px-[calc(var(&#45;&#45;gap-x)/2)] mb-[&#45;&#45;gap-y] w-[calc(var(&#45;&#45;size-xs)/12*100%)] md:w-[calc(var(&#45;&#45;size)/12*100%)]"-->
-<!--        :style="{ '&#45;&#45;size': layout?.size ?? 12, '&#45;&#45;size-xs': layout?.sizeXS ?? 12 }"-->
-<!--    >-->
-<!--        <slot></slot>-->
-<!--    </div>-->
--- resources/js/components/ui/StateIcon.vue
+++ resources/js/components/ui/StateIcon.vue
@@ -10,7 +10,6 @@
     <div class="relative rounded-full" :style="{ '--color': stateValue?.color ?? 'black' }"
         style="--border: oklch(from var(--color) .9 min(calc(c * .2), 0.03) h);"
     >
-<!--        <div class="absolute inset-0 rounded-full bg-[var(&#45;&#45;color)] opacity-10"></div>-->
         <div class="h-1 w-1 rounded-full bg-(--color) outline outline-(--border)"></div>
     </div>
 </template>
