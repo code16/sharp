9682838831e985d5a57202ae61003bad7bdb9fef
antoine <guingand.antoine@gmail.com>
2026-07-01 17:12:40 +0200

    fix update assets with controller + add test

--- src/SharpInternalServiceProvider.php
+++ src/SharpInternalServiceProvider.php
@@ -198,8 +198,8 @@ class SharpInternalServiceProvider extends ServiceProvider
     {
         Event::listen(CommandStarting::class, function (CommandStarting $event) {
             if ($event->command === 'vendor:publish'
-                && $event->input->getOption('tag')
-                && in_array('sharp-assets', $event->input->getOption('tag'))
+                && $event->input->hasOption('tag')
+                && collect($event->input->getOption('tag'))->contains('sharp-assets')
             ) {
                 if (File::exists(public_path('vendor/sharp'))) {
                     File::deleteDirectory(public_path('vendor/sharp'));
