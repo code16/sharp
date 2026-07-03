43fe938aa5a99f7edb584fe676e97e55022fe641
antoine <guingand.antoine@gmail.com>
2026-07-03 15:05:54 +0200

    Version

--- src/SharpInternalServiceProvider.php
+++ src/SharpInternalServiceProvider.php
@@ -56,7 +56,7 @@ use Laravel\Octane\Events\TickReceived;
 
 class SharpInternalServiceProvider extends ServiceProvider
 {
-    const VERSION = '10.0.0-beta.1';
+    const VERSION = '10.0.0-beta.2';
 
     public function boot()
     {
